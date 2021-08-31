70370b94db670e40ebb27c32ce6b6646a1aa73e6
sajald <sazzaldulal@gmail.com>
2021-08-31 20:26:25 +0545

    add support for jest in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
+		"jest/globals": true
 	},
 	extends: [
 		'plugin:react/recommended',
 		'xo',
+		'jest',
 	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
