a96b9e32290b1cf94228f38a6320e18b26771145
sajald <sazzaldulal@gmail.com>
2021-09-15 20:25:59 +0545

    updated button shadows

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,7 +1,7 @@
 export const styles = {
 	buttonCommon: {
-		'box-shadow': ' 0px 5px 7px 3px rgba(0,0,0,0.32)',
-		'-webkit-box-shadow': ' 0px 5px 7px 3px rgba(0,0,0,0.32)',
-		'-moz-box-shadow': ' 0px 5px 7px 3px rgba(0,0,0,0.32)',
+		'box-shadow': '0px 2px 4px 1px rgba(0,0,0,0.2)',
+		'-webkit-box-shadow': '0px 2px 4px 1px rgba(0,0,0,0.2)',
+		'-moz-box-shadow': '0px 2px 4px 1px rgba(0,0,0,0.2)',
 	},
 };
