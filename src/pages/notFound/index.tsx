60fedcd380839e5c1cb8e8931442d2cc320792a3
sajald <sazzaldulal@gmail.com>
2022-01-12 19:36:33 +0545

    add page not found feature

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,15 +1,24 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 import { BiErrorAlt } from 'react-icons/bi';
 
 export const NotFound = () => (
-	<Box width="100%" backgroundColor="brand.bgGrey" display="flex" justifyContent="center" alignItems="center">
-		<BiErrorAlt fontSize="40px"/>
-		<Text>
+	<VStack
+		width="100%"
+		height="100%"
+		backgroundColor="brand.bgGrey"
+		display="flex"
+		flexDirection="column"
+		justifyContent="center"
+		alignItems="center"
+		spacing="20px"
+	>
+		<BiErrorAlt fontSize="80px"/>
+		<Text fontSize="20px">
             Oops!
 		</Text>
-		<Text>
+		<Text fontSize="20px">
             This page was not found.
 		</Text>
-	</Box>
+	</VStack>
 );
