f607fa1448178140913f55b4a734dffe5398a742
Sajal Dulal <sazzaldulal@gmail.com>
2022-02-15 23:10:00 +0545

    Feature/mobile project  view (#25)
    * update twitter card details from navbar
    
    * Update project card #17 (#23) (#24)
    
    * update twitter card details from navbar
    
    * update project card
    
    * minor fix
    
    * added new buttons and animations in mobile view
    
    * minor adjustments to layout styles
    
    * minor detail update

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -13,6 +13,11 @@ export const getShortAmountLabel = (amount: number) => {
 
 	if (amount > 1000000 && amount < 1000000000) {
 		const newAmount = Math.round(amount / 1000000);
+		return `${newAmount}M`;
+	}
+
+	if (amount > 1000000000 && amount < 1000000000000) {
+		const newAmount = Math.round(amount / 1000000);
 		return `${newAmount}B`;
 	}
 };
