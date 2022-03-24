5d3487854c5fe169c15a8de86516dd64dcf77644
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-03-24 17:00:13 -0600

    feat: add succes page to add recipient modal

--- src/pages/grants/Grants.tsx
+++ src/pages/grants/Grants.tsx
@@ -58,6 +58,7 @@ const ContributeButton = () => {
 								inputMode="numeric"
 								focusBorderColor="#20ECC7"
 								min={1}
+								isRequired={true}
 							>
 								<NumberInputField placeholder={'sats'} />
 								<NumberInputStepper>
@@ -178,7 +179,65 @@ const ContributeButton = () => {
 };
 
 const RecipientButton = () => {
+	const [step, setStep] = useState(0);
+	const [recipient, setRecipient] = useState('');
+	const [link, setLink] = useState('');
 	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const addRecipient = (event:React.ChangeEvent<HTMLInputElement>) => setRecipient(event.target.value);
+
+	const addLink = (event:React.ChangeEvent<HTMLInputElement>) => setLink(event.target.value);
+
+	if (step === 0) {
+		return (
+			<>
+				<ButtonComponent
+					borderRadius="4px"
+					backgroundColor="brand-bgGrey2"
+					mb={2}
+					onClick={onOpen}
+				>
+				Submit a potential recipient
+				</ButtonComponent>
+
+				<Modal onClose={onClose} isOpen={isOpen} isCentered>
+					<ModalOverlay />
+					<ModalContent>
+						<ModalHeader>Submit request for a  grant recipient</ModalHeader>
+						<ModalCloseButton onClick={() => {
+							setRecipient('');
+							setLink('');
+							onClose();
+						}} />
+						<ModalBody>
+							<Text>Drop below the name, and a Tweet or link that demonstrates the person or organization’s fit to receive this grant.</Text>
+							<Text mt={5}>Name</Text>
+							<Input
+								name="name"
+								placeholder="Recipient"
+								focusBorderColor="#20ECC7"
+								onChange={addRecipient}
+								isRequired={true}
+							/>
+							<Text mt={5}>Link</Text>
+							<Input
+								name="link"
+								placeholder="https://twitter.com/metamick14"
+								focusBorderColor="#20ECC7"
+								onChange={addLink}
+								value={link}
+								isRequired={true}
+							/>
+						</ModalBody>
+						<ModalFooter>
+							<ButtonComponent primary width="100%" onClick={() => setStep(1)} disabled={link.length === 0 || recipient.length === 0}>Nominate</ButtonComponent>
+						</ModalFooter>
+					</ModalContent>
+				</Modal>
+			</>
+		);
+	}
+
 	return (
 		<>
 			<ButtonComponent
@@ -193,25 +252,23 @@ const RecipientButton = () => {
 			<Modal onClose={onClose} isOpen={isOpen} isCentered>
 				<ModalOverlay />
 				<ModalContent>
-					<ModalHeader>Submit request for a  grant recipient</ModalHeader>
-					<ModalCloseButton />
+					<ModalHeader>Success</ModalHeader>
+					<ModalCloseButton onClick={() => {
+						setRecipient('');
+						setLink('');
+						setStep(0);
+						onClose();
+					}} />
 					<ModalBody>
-						<Text>Drop below the name, and a Tweet or link that demonstrates the person or organization’s fit to receive this grant.</Text>
-						<Text mt={5}>Name</Text>
-						<Input
-							name="name"
-							placeholder="Recipient"
-							focusBorderColor="#20ECC7"
-						/>
-						<Text mt={5}>Link</Text>
-						<Input
-							name="link"
-							placeholder="https://twitter.com/metamick14"
-							focusBorderColor="#20ECC7"
-						/>
+						<Text>You nominated <b>{recipient}</b> to become a potential grant recipient.</Text>
 					</ModalBody>
 					<ModalFooter>
-						<ButtonComponent primary width="100%" >Nominate</ButtonComponent>
+						<ButtonComponent primary width="100%" onClick={() => {
+							setRecipient('');
+							setLink('');
+							setStep(0);
+							onClose();
+						}} >Close</ButtonComponent>
 					</ModalFooter>
 				</ModalContent>
 			</Modal>
