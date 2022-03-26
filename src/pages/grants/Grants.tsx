fe8a0493add8f9a79a4b37f79ce7c6830965cd79
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-03-26 16:39:29 -0600

    feat: random avatars for anons, update bubble styling, del whitespace

--- src/pages/grants/Grants.tsx
+++ src/pages/grants/Grants.tsx
@@ -11,6 +11,31 @@ import { SatoshiIcon, ArrowDownIcon, ArrowUpIcon } from '../../components/icons'
 import { Blob } from 'react-blob';
 import AnimatedCursor from 'react-animated-cursor';
 
+import Ellipse42 from '../../assets/random-avatars/Ellipse42.svg';
+import Ellipse43 from '../../assets/random-avatars/Ellipse43.svg';
+import Ellipse44 from '../../assets/random-avatars/Ellipse44.svg';
+import Ellipse45 from '../../assets/random-avatars/Ellipse45.svg';
+import Ellipse46 from '../../assets/random-avatars/Ellipse46.svg';
+import Ellipse47 from '../../assets/random-avatars/Ellipse47.svg';
+import Ellipse48 from '../../assets/random-avatars/Ellipse48.svg';
+import Ellipse49 from '../../assets/random-avatars/Ellipse49.svg';
+import Ellipse50 from '../../assets/random-avatars/Ellipse50.svg';
+import Ellipse51 from '../../assets/random-avatars/Ellipse51.svg';
+import Ellipse52 from '../../assets/random-avatars/Ellipse52.svg';
+import Ellipse53 from '../../assets/random-avatars/Ellipse53.svg';
+import Ellipse54 from '../../assets/random-avatars/Ellipse54.svg';
+import Ellipse55 from '../../assets/random-avatars/Ellipse55.svg';
+import Ellipse56 from '../../assets/random-avatars/Ellipse56.svg';
+import Ellipse57 from '../../assets/random-avatars/Ellipse57.svg';
+import Ellipse58 from '../../assets/random-avatars/Ellipse58.svg';
+import Ellipse59 from '../../assets/random-avatars/Ellipse59.svg';
+import Ellipse60 from '../../assets/random-avatars/Ellipse60.svg';
+import Ellipse61 from '../../assets/random-avatars/Ellipse61.svg';
+import Ellipse62 from '../../assets/random-avatars/Ellipse62.svg';
+import Ellipse63 from '../../assets/random-avatars/Ellipse63.svg';
+import Ellipse64 from '../../assets/random-avatars/Ellipse64.svg';
+import Ellipse65 from '../../assets/random-avatars/Ellipse65.svg';
+
 import { getDaysAgo, isMobileMode, isMediumScreen } from '../../utils';
 import useWindowSize from 'react-use/lib/useWindowSize';
 // import { QUERY_GET_PROJECT } from '../../graphql';
@@ -25,6 +50,8 @@ export const Grants = ({ project }: { project: IProject }) => {
 	const { width, height } = useWindowSize();
 	const { owners, funders, sponsors, grantees } = project;
 	const [sats, setSats] = useState(0);
+
+	const randomAvatars = [Ellipse42, Ellipse43, Ellipse44, Ellipse45, Ellipse46, Ellipse47, Ellipse48, Ellipse49, Ellipse50, Ellipse51, Ellipse52, Ellipse53, Ellipse54, Ellipse55, Ellipse56, Ellipse57, Ellipse58, Ellipse59, Ellipse60, Ellipse61, Ellipse62, Ellipse63, Ellipse64, Ellipse65];
 
 	return (
 		<>
@@ -82,9 +109,9 @@ export const Grants = ({ project }: { project: IProject }) => {
 								}}>
 									<Blob id="blob" size="21vh" onMouseDown={() => setSats(sats + 1000)}
 										style={{
-											backgroundImage: 'radial-gradient(rgba(32, 236, 199), rgba(27, 213, 179), #5B5B5B)',
+											backgroundImage: 'radial-gradient(ellipse at right, rgba(32, 236, 199), rgba(27, 213, 179), #E9E9E9)',
 											margin: '0 auto',
-											boxShadow: '0px 0px 21px #1BD5B3',
+											boxShadow: '0px 0px 30px 10px rgba(91, 91, 91, 0.25)',
 										}}
 									/>
 								</Box>
@@ -167,7 +194,7 @@ export const Grants = ({ project }: { project: IProject }) => {
 									<ClickableAvatar
 										key={funder.user.id}
 										url={`https://twitter.com/${funder.user.twitterHandle}`}
-										imageUrl={funder.user.imageUrl}
+										imageUrl={funder.user.username === 'anonymous' ? randomAvatars[Math.floor(Math.random() * 25)] : funder.user.imageUrl}
 									/>
 								))
 							}
