027c1c45f720b4a9f5947b7becac516923d13572
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-03-27 11:14:22 -0600

    style: update avatar tooltip wording

--- src/pages/grants/components/ClickableAvatar.tsx
+++ src/pages/grants/components/ClickableAvatar.tsx
@@ -3,7 +3,7 @@ import { Link, Avatar, Tooltip } from '@chakra-ui/react';
 
 export const ClickableAvatar = ({ url, imageUrl, amount, comment }: { url: string, imageUrl: string, amount: string, comment: string }) => (
 
-	<Tooltip label={comment ? `Amount: ${amount} sat Comment: ${comment}` : `Amount: ${amount} sat`} placement="top" bg="brand.bgGrey" color="black" borderRadius="base" hasArrow py={2}>
+	<Tooltip label={comment ? `${amount} sats "${comment}"` : `${amount} sats`} placement="top" bg="brand.bgGrey" color="black" borderRadius="base" hasArrow py={2}>
 		<Link href={url} isExternal p={1}>
 			<Avatar w={['40px', '60px']} h={['40px', '60px']} src={imageUrl} />
 		</Link>
