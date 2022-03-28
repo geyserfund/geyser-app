cb9f08d1d3cf443f16422866a4df30dbf61e85b0
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-03-27 23:36:16 -0600

    feat: hook up grant card to backend data

--- src/pages/grants/components/ComingSoon.tsx
+++ src/pages/grants/components/ComingSoon.tsx
@@ -6,7 +6,7 @@ title: string
 }
 
 export const ComingSoon = ({title}:ComingSoonProps) => (
-	<Box backgroundColor="white" p={10} boxShadow="lg" rounded="md" opacity="0.5" width="20rem" height="20.5rem" display="flex" justifyContent="center" alignItems="center" m={10}>
+	<Box backgroundColor="white" p={10} boxShadow="lg" rounded="md" opacity="0.5" width="20rem" height="23.75rem" display="flex" justifyContent="center" alignItems="center" m={10}>
 		<Box>
 			<Text fontSize="2xl" fontWeight="bold">{title}</Text>
 			<Text fontSize="lg" fontWeight="bold" color="brand.darkerPrimary">Coming soon</Text>
