b621bb8ef78ca97f1aa94a1d929fe2e63f8092bc
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-04-30 11:13:08 -0600

    style: updates to badge tooltip design

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -7,36 +7,36 @@ export const Badge = ({ badge }: { badge: string }) => {
 		<Box position="relative" as="span" backgroundColor="brand.bgGold" padding="0px 8px" borderRadius="20px" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
 			{hover
 && <>
-	<Box position="absolute" top="-160px" left="-164px" p={5} zIndex={2} bg="black" rounded="lg">
-		<Text color="white" textAlign="center" fontWeight="bold" fontSize="lg">Emoji badges</Text>
-		<HStack mt={3}>
-			<VStack rounded="lg" bg="#131313" py={1} px={6}>
+	<Box position="absolute" top="-102px" left="-93px" p={2} zIndex={2} bg="#5B5B5B" rounded="lg">
+		<Text color="white" fontWeight="bold">Emoji badges</Text>
+		<HStack mt={1}>
+			<VStack rounded="lg" bg="rgba(0, 0, 0, 0.11)" px={3}>
 				<Box>
 					🏅
 				</Box>
-				<Text color="white" fontWeight="bold">$10</Text>
+				<Text color="white" fontSize="8px">$10</Text>
 			</VStack>
-			<VStack rounded="lg" bg="#131313" py={1} px={5}>
+			<VStack rounded="lg" bg="rgba(0, 0, 0, 0.11)" px={3}>
 				<Box>
 					🏆
 				</Box>
-				<Text color="white" fontWeight="bold">$100</Text>
+				<Text color="white" fontSize="8px">$100</Text>
 			</VStack>
-			<VStack rounded="lg" bg="#131313" py={1} px={5}>
+			<VStack rounded="lg" bg="rgba(0, 0, 0, 0.11)" px={3}>
 				<Box>
 					👑
 				</Box>
-				<Text color="white" fontWeight="bold">$300</Text>
+				<Text color="white" fontSize="8px">$300</Text>
 			</VStack>
-			<VStack rounded="lg" bg="#131313" py={1} px={5}>
+			<VStack rounded="lg" bg="rgba(0, 0, 0, 0.11)" px={3}>
 				<Box>
 					⭐
 				</Box>
-				<Text color="white" fontWeight="bold">$500</Text>
+				<Text color="white" fontSize="8px">$500</Text>
 			</VStack>
 		</HStack>
 	</Box>
-	<Box position="absolute" top="-25px" left="-5px" zIndex={2} borderLeft="20px solid transparent" borderRight="20px solid transparent" borderTop="20px solid black"/>
+	<Box position="absolute" top="-25px" left="-5px" zIndex={2} borderLeft="20px solid transparent" borderRight="20px solid transparent" borderTop="20px solid #5B5B5B"/>
 </>
 			}
 			<Text fontSize="10px">{badge}</Text>
