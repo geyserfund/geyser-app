647d6a4285415584252082f66cf5fc1a38c22576
Stelios Rammos <stelio.rammos@gmail.com>
2022-05-20 20:59:53 +0200

    fix: identation

--- src/utils/hasShipping.ts
+++ src/utils/hasShipping.ts
@@ -1,7 +1,7 @@
 export const hasShipping = (name: string) => (
 	name !== 'day-of-genesis'
-    && name !== 'lightning-rebel'
-    && name !== 'bitcoin-ballers'
-    && name !== 'anatomy-of-bitcoin'
-		&& name !== 'bitcoin-for-fairness'
+  && name !== 'lightning-rebel'
+  && name !== 'bitcoin-ballers'
+  && name !== 'anatomy-of-bitcoin'
+  && name !== 'bitcoin-for-fairness'
 );
