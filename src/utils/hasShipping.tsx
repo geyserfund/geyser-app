ef5fe0fa9f63393da8bda92a1b13bb0dbed99992
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-05-20 12:54:35 -0600

    refactor: remove duplicate file

--- src/utils/hasShipping.tsx
+++
@@ -1,7 +0,0 @@
-export const hasShipping = (name: string) => (
-	name !== 'day-of-genesis'
-    && name !== 'lightning-rebel'
-    && name !== 'bitcoin-ballers'
-    && name !== 'anatomy-of-bitcoin'
-    && name !== 'bitcoin-for-fairness'
-);
