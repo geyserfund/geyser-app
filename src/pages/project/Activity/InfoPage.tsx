1ec59e909d343b461c8ae91cd870c70d18853a9c
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-05-25 10:18:20 -0600

    style: make bars full width to match section below

--- src/pages/project/Activity/InfoPage.tsx
+++ src/pages/project/Activity/InfoPage.tsx
@@ -80,15 +80,15 @@ export const InfoPage = ({
 				Fund this project
 			</ButtonComponent>}
 			<Box width="100%" display="flex" flexDirection="column" alignItems="center" overflow="hidden" flex="1">
-				<Box display="flex" marginBottom="10px">
+				<Box display="flex" marginBottom="10px" w="95%">
 					<Box w="50%">
-						<Button _hover={{backgroundColor: 'none'}} w="100%" rounded="md" bg="none" fontWeight={view === 'activity' ? 'bold' : 'normal'} fontSize="16px" marginTop="10px" onClick={() => setView('activity')}>
+						<Button _hover={{backgroundColor: 'none'}} w="100%" rounded="none" bg="none" fontWeight={view === 'activity' ? 'bold' : 'normal'} fontSize="16px" marginTop="10px" onClick={() => setView('activity')}>
 						Activity <Text ml={2} bg="brand.bgGrey" rounded="lg" px={3} py={1}>{fundingTxs.length}</Text>
 						</Button>
 						<Box bg={view === 'activity' ? 'darkgrey' : 'lightgrey'} w="100%" h="3px" rounded="lg"></Box>
 					</Box>
 					<Box w="50%">
-						<Button _hover={{backgroundColor: 'none'}} w="100%" rounded="md" bg="none" fontWeight={view === 'activity' ? 'normal' : 'bold'} fontSize="16px" marginTop="10px" onClick={() => setView('leaderboard')}>
+						<Button _hover={{backgroundColor: 'none'}} w="100%" rounded="none" bg="none" fontWeight={view === 'activity' ? 'normal' : 'bold'} fontSize="16px" marginTop="10px" onClick={() => setView('leaderboard')}>
 						Leaderboard <Text ml={2} bg="brand.bgGrey" rounded="lg" px={3} py={1}>{funders.length}</Text>
 						</Button>
 						<Box bg={view === 'activity' ? 'lightgrey' : 'darkgrey'} w="100%" h="3px" rounded="lg"></Box>
