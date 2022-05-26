3e7f712e22e77e34174549461c426f89a2ce0a6d
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-05-26 14:06:26 -0600

    feat: ellipse username if too long with or without badges to fit 7 digit

--- src/components/ui/LinkableAvatar.tsx
+++ src/components/ui/LinkableAvatar.tsx
@@ -8,16 +8,29 @@ interface ILinkableAvatar {
 	badges?: ReactElement[]
 }
 
-export const LinkableAvatar = ({ avatarMetadata, badges }: ILinkableAvatar) => (
-	<Link href={avatarMetadata.link} cursor={avatarMetadata.link ? 'pointer' : 'normal'} isExternal style={{ textDecoration: 'none' }}>
-		<HStack spacing="5px" display="flex">
-			<Avatar width="30px" height="30px" name={avatarMetadata.username} src={avatarMetadata.image} sx={{
-				'& .chakra-avatar__initials': {
-					lineHeight: '30px',
-				},
-			}} />
-			<Text fontSize="16px"> {avatarMetadata.username}</Text>
-			{badges}
-		</HStack>
-	</Link>
-);
+export const LinkableAvatar = ({ avatarMetadata, badges }: ILinkableAvatar) => {
+	let badgesLength = 0;
+	const calculateBadgesLength = () => {
+		if (badges) {
+			badges.forEach(element => {
+				badgesLength = element.props.badge.length + badgesLength;
+			});
+		}
+	};
+
+	calculateBadgesLength();
+
+	return (
+		<Link href={avatarMetadata.link} cursor={avatarMetadata.link ? 'pointer' : 'normal'} isExternal style={{ textDecoration: 'none' }}>
+			<HStack spacing="5px" display="flex">
+				<Avatar width="30px" height="30px" name={avatarMetadata.username} src={avatarMetadata.image} sx={{
+					'& .chakra-avatar__initials': {
+						lineHeight: '30px',
+					},
+				}} />
+				<Text fontSize="16px"> {badges && badges.length && avatarMetadata && avatarMetadata.username ? avatarMetadata.username.length + badgesLength > 22 ? `${avatarMetadata.username.slice(0, 4)}...` : avatarMetadata.username : avatarMetadata && avatarMetadata.username && avatarMetadata.username.length > 25 ? `${avatarMetadata.username.slice(0, 23)}...` : avatarMetadata.username}</Text>
+				{badges}
+			</HStack>
+		</Link>
+	);
+};
