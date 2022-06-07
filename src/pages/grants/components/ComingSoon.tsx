a006b0e23caeb5617c4b706f734647029652b462
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-06-07 17:40:10 -0600

    refactor: update coming soon grant component

--- src/pages/grants/components/ComingSoon.tsx
+++ src/pages/grants/components/ComingSoon.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Image } from '@chakra-ui/react';
+import { isMobileMode } from '../../../utils';
 
 interface ComingSoonProps {
-title: string
+image: string,
+number: string,
+date: string,
+title: string,
 }
 
-export const ComingSoon = ({title}:ComingSoonProps) => (
-	<Box backgroundColor="white" p={10} boxShadow="lg" rounded="md" opacity="0.5" width="20rem" height="23.75rem" display="flex" justifyContent="center" alignItems="center" m={10}>
-		<Box>
-			<Text fontSize="2xl" fontWeight="bold">{title}</Text>
-			<Text fontSize="lg" fontWeight="bold" color="brand.darkerPrimary">Coming soon</Text>
-		</Box>
-	</Box>
+export const ComingSoon = ({image, number, date, title}:ComingSoonProps) => {
+	const isMobile = isMobileMode();
+	return (
 
-);
+		<Box backgroundColor="white" p={2} boxShadow="lg" rounded="md" m={10}>
+
+			<Box w={isMobile ? '275px' : '400px'}>
+				<Image w={isMobile ? '275px' : '400px'} h={isMobile ? '275px' : '400px'} objectFit="cover" opacity={0.5} src={image} alt="grant" />
+
+				<Text fontSize="xs" fontWeight="medium" color="#6E6E6E" my={2} opacity={0.5}>GRANT {number}: {date}</Text>
+
+				<Text fontWeight="bold" fontSize="3xl" opacity={0.5}>{title}</Text>
+
+				<Box bg="brand.bgGrey3" w="400px" h="100px" rounded="sm" mb={3} opacity={0.5}></Box>
+				<Box bg="brand.bgGrey3" w="400px" h="100px" rounded="sm" mb={3} opacity={0.5}></Box>
+				<Box bg="brand.bgGrey3" w="400px" h="50px" rounded="sm" opacity={0.5}></Box>
+
+			</Box>
+
+		</Box>
+
+	);
+};
