9c30c6219c110cb1e9fb4cc4709137e958a9c763
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-06-07 17:41:17 -0600

    refactor: grant card on landing page

--- src/pages/grants/components/GrantCard.tsx
+++ src/pages/grants/components/GrantCard.tsx
@@ -1,81 +1,115 @@
+/* eslint-disable radix */
+
 import React, { useEffect } from 'react';
-import { Box, Text, HStack } from '@chakra-ui/react';
+import { Box, Text, HStack, Image } from '@chakra-ui/react';
 import { useHistory } from 'react-router';
-import { Blob } from 'react-blob';
 import { SatoshiIcon } from '../../../components/icons';
 import { QUERY_PROJECT_BY_NAME } from '../../../graphql';
-import { useQuery } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
 import Loader from '../../../components/ui/Loader';
 import { useNotification } from '../../../utils';
+import { RecipientButton } from './RecipientButton';
+import { isMobileMode } from '../../../utils';
 
 interface GrantCardProps {
-title: string,
 link: string,
-color1: string,
-color2: string,
-color3: string,
 number: string,
-status: string,
 }
 
-export const GrantCard = ({link, color1, color2, color3, number, status, title}:GrantCardProps) => {
+export const GrantCard = ({link, number}:GrantCardProps) => {
 	const history = useHistory();
 	const { toast } = useNotification();
+	const isMobile = isMobileMode();
 
 	const gotoGrant = () => {
 		history.push(`/project/${link}`);
 	};
 
-	const { loading: projectsLoading, error, data } = useQuery(QUERY_PROJECT_BY_NAME,
+	const [getProject, { loading, error, data }] = useLazyQuery(QUERY_PROJECT_BY_NAME,
 		{
-			variables: { name: link },
+			variables: { where: { name: link } },
 		},
 	);
 
-	const project = (data && data.getProjectByName && data.getProjectByName.project) || {};
-
 	useEffect(() => {
-		if (error) {
-			toast({
-				title: 'Something went wrong',
-				description: 'Please refresh the page',
-				status: 'error',
-			});
+		try {
+			getProject();
+		} catch {
+			if (error) {
+				toast({
+					title: 'Something went wrong',
+					description: 'Please refresh the page',
+					status: 'error',
+				});
+			}
 		}
-	}, [error]);
+	}
+	, []);
+
+	let project = (data && data.project && data) || {};
+
+	console.log(project);
+	console.log('project.title:', project.active);
+
+	project = {title: 'Bitcoin Education in Emerging Markets', description: 'Without the hard work of Bitcoin educators many of us today would not understand what is Bitcoin and why it’s important. With this grant we support people who are currently working on open source initiatives focused on Bitcoin education where it’s most needed: in emerging markets that are more often than not ravaged by inflation and depreciating savings. Yes, Bitcoin fixes this!', active: true, createdAt: Date.now(), sponsors: [{image: 'https://pbs.twimg.com/profile_images/1392218891322077192/16qp8f9S_400x400.jpg', id: 1}, {image: 'https://pbs.twimg.com/profile_images/1392218891322077192/16qp8f9S_400x400.jpg', id: 1}], media: ['https://storage.googleapis.com/geyser-projects-media/project/king/king_7.png']};
+
+	const showCountdown = () => project.active && project.expiresAt;
+	const daysLeft = Math.floor((parseInt(project.expiresAt) - Date.now()) / 86400000);
+	const createdDate = new Date(parseInt(project.createdAt)).toLocaleDateString();
 
 	return (
 
-		<Box backgroundColor="white" p={10} boxShadow="lg" rounded="md" m={10} onClick={() => gotoGrant()} cursor="pointer">
+		<Box backgroundColor="white" p={2} boxShadow="lg" rounded="md" m={10}>
 
-			<Blob id="blob" size="8rem"
-				style={{
-					backgroundImage: `radial-gradient(ellipse at right, ${color1}, ${color2}, ${color3})`,
-					margin: '0 auto',
-					boxShadow: '0px 0px 30px 10px rgba(91, 91, 91, 0.25)',
-				}}
-			/>
+			<Box w={isMobile ? '275px' : '400px'}>
+				{loading ? <Loader width="100%" h="800px"/>
+					: <>
+						<Box onClick={() => gotoGrant()} cursor="pointer">
+							<Image w={isMobile ? '275px' : '400px'} h={isMobile ? '275px' : '400px'} objectFit="cover" src={project.media && project.media[0]} alt="grant" />
 
-			<HStack spacing="10px" mt={10}>
-				<Text bg="brand.bgGrey" px={5} py={2} m={1} borderRadius="lg" fontWeight="bold">{number}</Text>
-				<Text fontSize="lg" fontWeight="bold" color="brand.darkerPrimary">Grant {status}</Text>
-			</HStack>
+							<Text fontSize="xs" fontWeight="medium" color="#6E6E6E" my={2}>GRANT {number}: {createdDate}</Text>
 
-			<Text fontWeight="bold" fontSize="2xl">{title}</Text>
+							<Text fontWeight="bold" fontSize="3xl">{project.title}</Text>
 
-			{projectsLoading ? <Loader width="50%"/>
-				: <>
-					<HStack>
-						<SatoshiIcon scale={0.8}/><Text fontWeight="bold">{`${project.balance}`}</Text>
-						<Text>raised</Text>
-					</HStack>
+							<Text>{project.description && project.description.length > 119 ? `${project.description.slice(0, 119)}...` : project.description}</Text>
 
-					<HStack>
-						<Text fontWeight="bold">{project.fundingTxs.length}</Text>
-						<Text>donations</Text>
-					</HStack>
-				</>
-			}
+							<HStack justifyContent="space-between" alignItems="center" my={3}>
+
+								<Box>
+									<HStack justifyContent="center">
+										<SatoshiIcon scale={0.8}/><Text fontWeight="bold">{project.balance}</Text>
+									</HStack>
+									<Text fontSize="sm" color="#5B5B5B">COMMITTED</Text>
+								</Box>
+
+								<Box>
+									<Text fontWeight="bold" textAlign="center">{project.grantees ? project.grantees.length : 0}</Text>
+									<Text fontSize="sm" color="#5B5B5B" mt={1}>APPLICANTS</Text>
+								</Box>
+
+								<Box>
+									<Box fontWeight="bold" textAlign="center">{showCountdown() ? daysLeft : 0}</Box>
+									<Text fontSize="sm" color="#5B5B5B" mt={1}>DAYS LEFT</Text>
+								</Box>
+
+							</HStack>
+
+							<Box>
+								<Text fontSize="sm" color="#5B5B5B" textAlign="center">SPONSORED BY</Text>
+								{project.sponsors
+									? <HStack justifyContent="center">
+										{project.sponsors.map((sponsor:any) => <Image src={sponsor.image} alt="sponsor" key={sponsor.id} w="25%"/>)}
+									</HStack>
+									: <Text textAlign="center">No sponsors yet.</Text>}
+							</Box>
+						</Box>
+						<Box display="flex" justifyContent="center" w="100%" mt={2}>
+							<RecipientButton title={`APPLICATIONS ${project.active ? 'OPEN' : 'CLOSED'}`} page="landing" active={project.active}/>
+						</Box>
+					</>
+				}
+			</Box>
+
 		</Box>
 
 	);
