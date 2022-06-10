648b2a2d5e4893fded59345bf34b39be3527cc97
Stelios Rammos <stelio.rammos@gmail.com>
2022-06-10 10:48:37 +0300

    fix: use bitstamp instead of coinbase api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import { REACT_APP_AIR_TABLE_KEY } from '../constants';
 
-export const fetchBitcoinRates = async () => fetch('https://api.coinbase.com/v2/exchange-rates?currency=BTC').then(response => response.json()).then(response => response.data);
+// Export const fetchBitcoinRates = async () => fetch('https://api.coinbase.com/v2/exchange-rates?currency=BTC').then(response => response.json()).then(response => response.data);
+export const fetchBitcoinRates = async () => fetch('https://api-pub.bitfinex.com/v2/ticker/tBTCUSD').then(response => response.json()).then(response => response.data[6]);
 
 export const createCreatorRecord = async (data:any) => {
 	fetch('https://api.airtable.com/v0/appyM7XlNIWVypuP5/Subscribers%20(Funders%2BCreators)', {
