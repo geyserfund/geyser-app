bc70959dc54bb27882f4fee4a99864f7cfcc43bc
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-06-11 21:01:26 -0600

    refactor: remove not needed div

--- src/pages/grants/components/GrantCard.tsx
+++ src/pages/grants/components/GrantCard.tsx
@@ -18,38 +18,37 @@ export const GrantCard = ({ project, number}: {project: IProject, number: string
 	const createdDate = new Date(parseInt(project.createdAt)).toLocaleDateString();
 
 	return (
-		<Box backgroundColor="white" boxShadow="0px 0px 10px rgba(0, 0, 0, 0.08)" rounded="md" my={10} mr={isMobile ? 10 : 20}>
+		<Box backgroundColor="white" boxShadow="0px 0px 10px rgba(0, 0, 0, 0.08)" rounded="md" my={10} mr={isMobile ? 10 : 20} onClick={() => gotoGrant()} cursor="pointer">
 
 			<Box w={isMobile ? '275px' : '400px'}>
 				<>
-					<Box onClick={() => gotoGrant()} cursor="pointer">
 
-						<Image w={isMobile ? '275px' : '400px'} h={isMobile ? '275px' : '400px'} objectFit="cover" src={project.media && project.media[0]} alt="grant" />
+					<Image w={isMobile ? '275px' : '400px'} h={isMobile ? '275px' : '400px'} objectFit="cover" src={project.media && project.media[0]} alt="grant" />
 
-						<Box p={2}>
+					<Box p={2}>
 
-							<Text fontWeight="bold" fontSize="3xl">{project.title}</Text>
-							<Text fontSize="xs" fontWeight="medium" color="#6E6E6E">ROUND {number}: {createdDate}</Text>
+						<Text fontWeight="bold" fontSize="3xl">{project.title}</Text>
+						<Text fontSize="xs" fontWeight="medium" color="#6E6E6E">ROUND {number}: {createdDate}</Text>
 
-							<HStack justifyContent="space-around" alignItems="center" my={3}>
+						<HStack justifyContent="space-around" alignItems="center" my={3}>
 
-								<Box>
-									<HStack justifyContent="center">
-										<SatoshiIcon scale={0.8} /><Text fontWeight="bold" fontSize="lg">{project.balance ? project.balance : 0}</Text>
-									</HStack>
-									<Text fontSize="sm" color="#5B5B5B" fontWeight="medium">CONTRIBUTED</Text>
-								</Box>
+							<Box>
+								<HStack justifyContent="center">
+									<SatoshiIcon scale={0.8} /><Text fontWeight="bold" fontSize="lg">{project.balance ? project.balance : 0}</Text>
+								</HStack>
+								<Text fontSize="sm" color="#5B5B5B" fontWeight="medium">CONTRIBUTED</Text>
+							</Box>
 
-								<Box>
-									<HStack justifyContent="center">
-										<SatoshiIcon scale={0.8} /><Text fontWeight="bold" fontSize="lg">{project.balance ? project.balance : 0}</Text>
-									</HStack>
-									<Text fontSize="sm" color="#5B5B5B" fontWeight="medium">DISTRIBUTED</Text>
-								</Box>
+							<Box>
+								<HStack justifyContent="center">
+									<SatoshiIcon scale={0.8} /><Text fontWeight="bold" fontSize="lg">{project.balance ? project.balance : 0}</Text>
+								</HStack>
+								<Text fontSize="sm" color="#5B5B5B" fontWeight="medium">DISTRIBUTED</Text>
+							</Box>
 
-							</HStack>
-						</Box>
+						</HStack>
 					</Box>
+
 				</>
 			</Box>
 		</Box>
