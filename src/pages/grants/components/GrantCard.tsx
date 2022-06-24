5b27d652b131398b6e122579c073d50030ecb8c3
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-06-24 11:19:23 -0600

    fix: move link overlay lower in DOM

--- src/pages/grants/components/GrantCard.tsx
+++ src/pages/grants/components/GrantCard.tsx
@@ -14,54 +14,54 @@ export const GrantCard = ({ project, number, distributed, date, status, marginRi
 	};
 
 	return (
-		<LinkBox>
-			<LinkOverlay href={`https://geyser.fund/project/${project.name}`} onClick={e => {
-				e.preventDefault();
-				gotoGrant();
-			}}>
-				<Box backgroundColor="white" _hover={{boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.08)'}} boxShadow="0px 0px 10px rgba(0, 0, 0, 0.08)" borderRadius="4px" my={10} mr={marginRight ? isMobile ? 10 : 20 : 0} cursor="pointer">
 
-					<Box w={isMobile ? '325px' : '350px'}>
-						<>
+		<Box backgroundColor="white" _hover={{boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.08)'}} boxShadow="0px 0px 10px rgba(0, 0, 0, 0.08)" borderRadius="4px" my={10} mr={marginRight ? isMobile ? 10 : 20 : 0} cursor="pointer">
 
-							<Image w={isMobile ? '325px' : '350px'} h={isMobile ? '325px' : '350px'} objectFit="cover" borderRadius="4px" src={project.media && project.media[0]} alt="grant" />
+			<Box w={isMobile ? '325px' : '350px'}>
+				<LinkBox>
+					<LinkOverlay href={`https://geyser.fund/project/${project.name}`} onClick={e => {
+						e.preventDefault();
+						gotoGrant();
+					}}>
 
-							<Box p={2}>
+						<Image w={isMobile ? '325px' : '350px'} h={isMobile ? '325px' : '350px'} objectFit="cover" borderRadius="4px" src={project.media && project.media[0]} alt="grant" />
 
-								<Text fontWeight="bold" fontSize="3xl">{project.title}</Text>
-								<Text fontSize="22px" fontWeight="medium">ROUND {number}: {date}</Text>
+						<Box p={2}>
 
-								<HStack justifyContent="center" spacing="40px" alignItems="center" my={3}>
+							<Text fontWeight="bold" fontSize="3xl">{project.title}</Text>
+							<Text fontSize="22px" fontWeight="medium">ROUND {number}: {date}</Text>
 
-									<Box>
-										<HStack justifyContent="center">
-											<SatoshiIconTilted /><Text fontWeight="bold" fontSize="lg">{(project.balance / 1000000).toFixed(project.balance === 0 ? 0 : 1)} M</Text>
-										</HStack>
-										<Text fontSize="md" color="#5B5B5B" fontWeight="bold">CONTRIBUTED</Text>
-									</Box>
+							<HStack justifyContent="center" spacing="40px" alignItems="center" my={3}>
 
-									<Box>
-										<HStack justifyContent="center">
-											<SatoshiIconTilted /><Text fontWeight="bold" fontSize="lg">{distributed} M</Text>
-										</HStack>
-										<Text fontSize="md" color="#5B5B5B" fontWeight="bold">DISTRIBUTED</Text>
-									</Box>
+								<Box>
+									<HStack justifyContent="center">
+										<SatoshiIconTilted /><Text fontWeight="bold" fontSize="lg">{(project.balance / 1000000).toFixed(project.balance === 0 ? 0 : 1)} M</Text>
+									</HStack>
+									<Text fontSize="md" color="#5B5B5B" fontWeight="bold">CONTRIBUTED</Text>
+								</Box>
 
-								</HStack>
+								<Box>
+									<HStack justifyContent="center">
+										<SatoshiIconTilted /><Text fontWeight="bold" fontSize="lg">{distributed} M</Text>
+									</HStack>
+									<Text fontSize="md" color="#5B5B5B" fontWeight="bold">DISTRIBUTED</Text>
+								</Box>
 
-								{status === 'pending'
-									? <Text width="100%" p={1} border="2px solid #20ECC7" rounded="md" textAlign="center" fontWeight="bold">COMING SOON</Text>
-									: status === 'open'
-										? <Text width="100%" p={1} bg="brand.primary" rounded="md" textAlign="center" fontWeight="bold">OPEN</Text>
-										:	<Text width="100%" p={1} bg="brand.bgGrey3" rounded="md" textAlign="center" fontWeight="bold">ROUND COMPLETED</Text>
-								}
+							</HStack>
 
-							</Box>
+							{status === 'pending'
+								? <Text width="100%" p={1} border="2px solid #20ECC7" rounded="md" textAlign="center" fontWeight="bold">COMING SOON</Text>
+								: status === 'open'
+									? <Text width="100%" p={1} bg="brand.primary" rounded="md" textAlign="center" fontWeight="bold">OPEN</Text>
+									:	<Text width="100%" p={1} bg="brand.bgGrey3" rounded="md" textAlign="center" fontWeight="bold">ROUND COMPLETED</Text>
+							}
 
-						</>
-					</Box>
-				</Box>
-			</LinkOverlay>
-		</LinkBox>
+						</Box>
+					</LinkOverlay>
+				</LinkBox>
+			</Box>
+
+		</Box>
+
 	);
 };
