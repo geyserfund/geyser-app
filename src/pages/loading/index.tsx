f73c8c7718849d9055687cb2f3e03f783af7fbda
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-08-09 00:15:16 -0600

    chore: remove console log

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -2,21 +2,18 @@ import { Box, Image, Progress, VStack } from '@chakra-ui/react';
 import React from 'react';
 import LogoNameLight from '../../assets/logo-name-brand.svg';
 
-export const LoadingPage = () => {
-	console.log('checking open ');
-	return (
-		<VStack
-			height="100vh"
-			width="100%"
-			color="brand.primary"
-			justifyContent="center"
-			alignItems="center"
-			spacing="20px"
-		>
-			<Image height="75px" src={LogoNameLight} alt="geyser logo image" objectFit="contain" />
-			{/* <Box maxWidth="285px" width="100%">
+export const LoadingPage = () => (
+	<VStack
+		height="100vh"
+		width="100%"
+		color="brand.primary"
+		justifyContent="center"
+		alignItems="center"
+		spacing="20px"
+	>
+		<Image height="75px" src={LogoNameLight} alt="geyser logo image" objectFit="contain" />
+		{/* <Box maxWidth="285px" width="100%">
 				<Progress size="xs" isIndeterminate colorScheme="teal" />
 			</Box> */}
-		</VStack>
-	);
-};
+	</VStack>
+);
