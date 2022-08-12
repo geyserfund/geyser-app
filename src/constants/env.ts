d304b581347be9c1ba4b7dc2c8239583643829e1
Stelios Rammos <stelio.rammos@gmail.com>
2022-08-12 21:22:31 +0200

    fix

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,13 +1,13 @@
 type ExtendProcessEnv = 'production' | 'staging' | 'development' | 'test'
 
-const REAT_APP_ENV = process.env.REAT_APP_ENV as ExtendProcessEnv;
+const REACT_APP_ENV = process.env.REACT_APP_ENV as ExtendProcessEnv;
 
-export const __production__ = REAT_APP_ENV === 'production';
-export const __development__ = REAT_APP_ENV === 'development';
-export const __staging__ = REAT_APP_ENV === 'staging';
+export const __production__ = REACT_APP_ENV === 'production';
+export const __development__ = REACT_APP_ENV === 'development';
+export const __staging__ = REACT_APP_ENV === 'staging';
 
 if (!(__staging__ || __production__ || __staging__)) {
-	throw new Error('REAT_APP_ENV variable has invalid value');
+	throw new Error('REACT_APP_ENV variable has invalid value');
 }
 
 export const { REACT_APP_API_ENDPOINT, REACT_APP_AIR_TABLE_KEY } = process.env;
