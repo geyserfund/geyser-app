85757c41fd11b42f5106b7408fae74bcca4f18ce
sajald77 <sazzaldulal@gmail.com>
2022-08-17 09:52:21 -0400

    fix: get post query

--- src/graphql/queries/posts.ts
+++ src/graphql/queries/posts.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const QUERY_GET_POST = gql`
 query Entry($id: BigInt!)  {
-	entry($id: BigInt!)  {
+	entry(id: $id)  {
 	  id
 	  title
 	  description
