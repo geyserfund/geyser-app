abc757798870f34019f20d0950e63967fda27de9
Stelios Rammos <stelio.rammos@gmail.com>
2022-08-19 00:41:17 +0200

    hot fix: cookies

--- src/constants/cookie.ts
+++ src/constants/cookie.ts
@@ -1,6 +1,6 @@
 import { __development__, __production__, __staging__ } from './env';
 
-const domain = (__production__ && 'geyser.fund')
+const domain = (__production__ && '.geyser.fund')
 	|| (__staging__ && 'staging.geyser.fund')
 	|| (__development__ && 'localhost')
 	|| 'dev.geyser.fund';
