c9353189ec3923475d6fa5f787e1f70a2ddc1263
Stelios Rammos <stelio.rammos@gmail.com>
2022-09-24 20:46:38 +0200

    add launch page

---
+++ src/components/molecules/TwitterConnect.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react';
+import { ButtonComponent } from '../ui';
+import { SiTwitter } from 'react-icons/si';
+import Icon from '@chakra-ui/icon';
+import { AUTH_SERVICE_ENDPOINT } from '../../constants';
+import { useAuthContext } from '../../context';
+import { useNotification, hasTwitterAccount } from '../../utils';
+import { useLazyQuery } from '@apollo/client';
+import { ME } from '../../graphql';
+
+export const TwitterConnect = ({ onClose }: { onClose?: () => {}}) => {
+	const { setUser, setIsLoggedIn } = useAuthContext();
+	const { toast } = useNotification();
+	const [getUser, { stopPolling }] = useLazyQuery(ME, {
+		onCompleted: (data: any) => {
+			if (data && data.me) {
+				console.log('DATA ME', data.me);
+				const hasTwitter = hasTwitterAccount(data.me);
+				if (hasTwitter) {
+					if (onClose !== undefined) {
+						onClose();
+					}
+
+					stopPolling();
+					setUser(data.me);
+					setIsLoggedIn(true);
+				}
+			}
+		},
+		fetchPolicy: 'network-only',
+		pollInterval: 1000,
+	});
+
+	const [pollAuthStatus, setPollAuthStatus] = useState(false);
+
+	useEffect(() => {
+		if (pollAuthStatus) {
+			const id = setInterval(async () => {
+				let statusRes;
+				try {
+					statusRes = await fetch(`${AUTH_SERVICE_ENDPOINT}/status`, { credentials: 'include', redirect: 'follow' });
+				} catch (error) {
+					stopPolling();
+					setPollAuthStatus(false);
+					toast({
+						title: 'Something went wrong',
+						description: `The authentication request failed: ${(error as Error).message}.`,
+						status: 'error',
+					});
+				}
+
+				if (statusRes && statusRes.status === 200) {
+					const { status: authStatus, reason } = await statusRes.json();
+					if (authStatus === 'success') {
+						setPollAuthStatus(false);
+					} else if (authStatus === 'failed') {
+						if (stopPolling) {
+							stopPolling();
+						}
+
+						setPollAuthStatus(false);
+						toast({
+							title: 'Something went wrong',
+							description: `The authentication request failed: ${reason}.`,
+							status: 'error',
+						});
+					}
+				}
+			}, 1000);
+
+			return () => clearInterval(id);
+		}
+	}, [pollAuthStatus]);
+
+	const handleClick = async () => {
+		try {
+			const response = await fetch(`${AUTH_SERVICE_ENDPOINT}/auth-token`, { credentials: 'include', redirect: 'follow' });
+
+			if (response.status >= 200 && response.status < 400) {
+				setPollAuthStatus(true);
+				getUser();
+				window.open(`${AUTH_SERVICE_ENDPOINT}/twitter?nextPath=/auth/twitter`, '_blank', 'noopener,noreferrer');
+			} else {
+				toast({
+					title: 'Something went wrong',
+					description: 'The authentication request failed: could not get authentication token.',
+					status: 'error',
+				});
+			}
+		} catch (err) {
+			console.log('err', err);
+		}
+	};
+
+	return (
+		<ButtonComponent
+			isFullWidth
+			primary
+			standard
+			leftIcon={<Icon as={SiTwitter} />}
+			onClick={handleClick}
+		>
+			Twitter
+		</ButtonComponent>
+	);
+};
