a74f1886bc748925fcb1504c86794aace9edbd78
Stelios Rammos <stelio.rammos@gmail.com>
2022-10-02 21:44:49 +0200

    rename projects query input variables

--- src/graphql/queries/project.ts
+++ src/graphql/queries/project.ts
@@ -164,8 +164,8 @@ export const QUERY_GRANTS = gql`
 `;
 
 export const QUERY_PROJECTS = gql`
-  query projects($where: ProjectQueryInput) {
-    projects(where: $where) {
+  query projects($input: ProjectsGetQueryInput) {
+    projects(input: $input) {
       projects {
         id
         title
