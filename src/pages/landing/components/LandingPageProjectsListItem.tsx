5fe65bfb3394fa51324e67733de69269f43301d3
CypherPoet <CypherPoet@tutanota.com>
2022-10-06 00:24:10 -0500

    Styling updates for the Landing Page "top projects" list.
    Connected to:
    - https://geyserteam.atlassian.net/browse/GT-448
    - https://geyserteam.atlassian.net/browse/GT-505

--- src/pages/landing/components/LandingPageProjectsListItem.tsx
+++ src/pages/landing/components/LandingPageProjectsListItem.tsx
@@ -1,4 +1,11 @@
-import { HStack, HTMLChakraProps, Text, Image, Link } from '@chakra-ui/react';
+import {
+  HStack,
+  HTMLChakraProps,
+  Text,
+  Image,
+  Link,
+  Box,
+} from '@chakra-ui/react';
 import React from 'react';
 import { IProject } from '../../../interfaces';
 import { Link as ReactRouterLink } from 'react-router-dom';
@@ -17,17 +24,22 @@ export const LandingPageProjectsListItem = ({ project, ...rest }: Props) => {
       to={`/projects/${project.name}`}
       _hover={{ textDecoration: 'none' }}
     >
-      <HStack spacing={3} display="flex" alignItems="center" {...rest}>
-        <Image
-          flexShrink={0}
-          src={projectImageSrc}
-          boxSize="42px"
-          borderRadius="md"
-          objectFit="cover"
-          fallback={<ProjectImageListItemPlaceholder />}
-          fit="cover"
-          alt={`Main image for ${project.name}`}
-        />
+      <HStack
+        spacing={3}
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-start"
+        {...rest}
+      >
+        <Box width="42px" height="42px" flexShrink={0}>
+          <Image
+            src={projectImageSrc}
+            borderRadius="md"
+            objectFit="cover"
+            fallback={<ProjectImageListItemPlaceholder />}
+            alt={`Main image for ${project.name}`}
+          />
+        </Box>
 
         <Text color="brand.neutral900" fontWeight={'bold'} isTruncated>
           {project.title}
