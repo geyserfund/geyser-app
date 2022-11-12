b045f34118076cfa475f84845fcfcdeaa6efebe3
CypherPoet <CypherPoet@tutanota.com>
2022-11-11 23:48:57 -0600

    add comment

--- src/config/apollo-client/customClientTypeDefs.ts
+++ src/config/apollo-client/customClientTypeDefs.ts
@@ -1,5 +1,11 @@
 import { gql } from '@apollo/client';
 
+/**
+ * Custom client-side extensions (AKA mocks) for mocking and
+ * augmenting the back-end schema
+ *
+ * See: https://www.apollographql.com/docs/react/development-testing/client-schema-mocking/
+ */
 export const customClientTypeDefs = gql`
   type LightningAddressConnectionDetailsCreateInput {
     lightningAddress: String!
