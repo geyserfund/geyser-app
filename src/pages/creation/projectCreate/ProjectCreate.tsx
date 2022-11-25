fa96fca3e23355a60edeadd904a13aae1f7cde25
CypherPoet <CypherPoet@tutanota.com>
2022-11-25 17:18:50 -0600

    Fix Navigation During Step 1 of Project Creation Flow
    Fixes https://geyserteam.atlassian.net/browse/GT-1014

--- src/pages/creation/projectCreate/ProjectCreate.tsx
+++ src/pages/creation/projectCreate/ProjectCreate.tsx
@@ -44,11 +44,11 @@ import { ProjectValidations } from '../../../constants/validations/project';
 import { UserValidations } from '../../../constants/validations';
 
 type CreateProjectMutationResponseData = {
-  createdProject: Project | null;
+  createProject: Project | null;
 };
 
 type UpdateProjectMutationResponseData = {
-  updatedProject: Project | null;
+  updateProject: Project | null;
 };
 
 const useStyles = createUseStyles({
@@ -84,7 +84,7 @@ export const ProjectCreate = () => {
     CreateProjectMutationResponseData,
     { input: ProjectCreationVariables }
   >(MUTATION_CREATE_PROJECT, {
-    onCompleted({ createdProject }) {
+    onCompleted({ createProject: createdProject }) {
       if (createdProject && createdProject.owners[0]) {
         const newOwnershipInfo = user.ownerOf.concat([
           {
@@ -100,7 +100,9 @@ export const ProjectCreate = () => {
           },
         });
 
-        history.push(`/launch/${createdProject.id}/milestones`);
+        history.push(
+          getPath('launchProjectWithMilestonesAndRewards', createdProject.id),
+        );
       }
     },
     onError(error) {
