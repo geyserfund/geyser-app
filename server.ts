9eff1f49cfee9a8fb39fd2fcf6d9748161415f23
Stelios Rammos <stelio.rammos@gmail.com>
2022-11-30 22:01:23 -0600

    fix port env

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const handler = require('serve-handler');
 const prerender = require('prerender-node');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(
