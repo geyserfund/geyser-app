12ef9b086e0d1872f2a133ff273348c5bb31232e
sajald77 <sazzaldulal@gmail.com>
2023-01-02 20:15:36 -0500

    fix: More information copy updates

--- src/pages/grants/components/MoreInfo.tsx
+++ src/pages/grants/components/MoreInfo.tsx
@@ -26,17 +26,19 @@ export const MoreInfo = ({ titleProps }: MoreInfoProps) => {
           fontSize={'14px'}
           textAlign="justify"
         >
-          Bitcoin is signal, everything else is noise. We created Geyser Grants
+          {`Bitcoin is signal, everything else is noise. We created Geyser Grants
           to help broadcast more Bitcoin signal into the world. That is, to
           accelerate the growth of the Bitcoin ecosystem by increasing Bitcoin
-          awarenes, enabling Bitcoin culture, and supporting needed development.
-          Through these grants we will be supporting Bitcoin educators,
-          developers, entrepreneurs and creatives with the resources they need
-          to bootstrap their initiatives. We accept Bitcoin contributions for
-          each individual grant and take no fees at this stage. When the Round
-          goes live applications will be opened, and they will be evaluated once
-          the Rounds close. Grants will be given away as soon as the goal is
-          reached. For more information see this doc.
+          awareness, enabling Bitcoin culture, and supporting needed
+          development. Through these grants we will be supporting Bitcoin
+          educators, developers, entrepreneurs and creatives with the resources
+          they need to bootstrap their initiatives. We accept Bitcoin
+          contributions for each individual grant and don't charge any grants
+          operations fees - Geyser charges no fee to node-running projects, and
+          2% to projects that use a lightning address. When the Round goes live
+          applications will be opened, and they will be evaluated once the
+          Rounds close. Grants will be given away as soon as the goal is
+          reached. For more information see this doc.`}
         </Text>
         <Box w={20} mt="4" mb={10}>
           <Link href={GrantsFAQUrl} isExternal>
