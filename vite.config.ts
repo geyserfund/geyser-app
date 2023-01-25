e66fd8e8746e767a846149f41a4cb9cb041192e2
sajald77 <sazzaldulal@gmail.com>
2023-01-25 14:57:52 -0500

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig, loadEnv } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 
-const PORT = 3000;
+const PORT = 3000
 
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
   const server = {
     port: Number(env.VITE_PORT),
     https: false,
     proxy: undefined,
     open: `http://dev.geyser.fund:${PORT}/`,
-  };
+  }
 
   if (mode === 'development') {
     console.log(`
       ==================================================================================================
       "Geyser - App" will available at http://dev.geyser.fund:${PORT}/
       ==================================================================================================
-      `);
+      `)
   }
 
   return {
@@ -35,5 +35,5 @@ export default defineConfig(({ command, mode }) => {
       environment: 'jsdom',
       setupFiles: './setupTests.ts',
     },
-  };
-});
+  }
+})
