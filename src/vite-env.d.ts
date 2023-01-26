f9be2bc39792f98e41084a69a116794968cdbe47
sajald77 <sazzaldulal@gmail.com>
2023-01-26 08:37:04 -0500

    fix: env fixes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,7 +5,7 @@ type ImportMetaEnv = {
   readonly VITE_APP_AIR_TABLE_KEY: string
   readonly VITE_APP_ENV: string
   readonly VITE_APP_GIPHY_API_KEY: string
-  readonly VITE_APP_APOLLO_KEY: string
+  readonly APOLLO_KEY: string
 }
 
 type ImportMeta = {
