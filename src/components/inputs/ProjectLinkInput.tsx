3ed96c130cbffc8180828211f16e0bd0ae2902eb
sajald77 <sazzaldulal@gmail.com>
2023-01-30 19:45:06 -0500

    fix: update designs of link input component

--- src/components/inputs/ProjectLinkInput.tsx
+++ src/components/inputs/ProjectLinkInput.tsx
@@ -2,6 +2,7 @@ import { CloseIcon } from '@chakra-ui/icons'
 import { Box, HStack, Input, InputProps, StackProps } from '@chakra-ui/react'
 import { IconType } from 'react-icons'
 
+import { colors } from '../../styles'
 import { IconButtonComponent } from '../ui'
 
 interface ProjectLinkInputProps extends StackProps {
@@ -26,29 +27,31 @@ export const ProjectLinkInput = ({
     <HStack
       w="100%"
       size="md"
-      padding="5px"
+      padding="12px 5px"
       backgroundColor="white"
       borderRadius="4px"
       border="1px solid"
       borderColor="brand.neutral200"
       {...rest}
     >
-      <HStack paddingX="12px" width="50px">
-        <LeftIcon />
+      <HStack justifyContent="center" width="50px">
+        <LeftIcon fontSize="20px" />
       </HStack>
       <Input
         border="1px solid"
         borderRadius="4px !important"
         borderColor={'brand.neutral400'}
-        backgroundColor="brand.neutral100"
-        placeholder="mysite"
+        placeholder="https://twitter.com/halfin"
         value={value}
         onChange={onChange}
         isInvalid={isError}
-        _focusVisible={{ borderColor: isError ? 'red' : 'brand.neutral600' }}
+        _focusVisible={{
+          borderColor: isError ? 'red' : colors.normalLightGreen,
+          boxShadow: `0 0 0 1px ${colors.normalLightGreen}`,
+        }}
         {...inputProps}
       />
-      <Box paddingX="10px" backgroundColor="white" border="none">
+      <Box paddingX="5px" backgroundColor="white" border="none">
         <IconButtonComponent
           variant="ghost"
           size="sm"
