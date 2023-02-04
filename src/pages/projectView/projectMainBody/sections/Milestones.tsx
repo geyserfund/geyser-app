fc820f783759053ade8394fc79f754397ee75492
sajald77 <sazzaldulal@gmail.com>
2023-02-03 20:08:50 -0500

    feat: add milestone add edit from project page feature

--- src/pages/projectView/projectMainBody/sections/Milestones.tsx
+++ src/pages/projectView/projectMainBody/sections/Milestones.tsx
@@ -1,16 +1,34 @@
-import { Text, VStack } from '@chakra-ui/react'
+import { Text, useDisclosure, VStack } from '@chakra-ui/react'
+import { BiPencil } from 'react-icons/bi'
 
 import { CardLayout } from '../../../../components/layouts'
 import { ProjectSectionBar } from '../../../../components/molecules'
+import { IconButtonComponent } from '../../../../components/ui'
 import { ID } from '../../../../constants'
 import { useProjectContext } from '../../../../context'
+import { Project, ProjectMilestone } from '../../../../types'
+import {
+  defaultMilestone,
+  MilestoneAdditionModal,
+} from '../../../creation/projectCreate/components'
 import { MilestoneComponent } from '../components/MilestoneComponent'
 
 export const Milestones = () => {
-  const { project } = useProjectContext()
+  const { project, isProjectOwner, updateProject } = useProjectContext()
 
   const hasMilestones = project.milestones && project.milestones.length > 0
   const milestoneLength = project.milestones ? project.milestones.length : 0
+
+  const {
+    isOpen: isMilestoneModalOpen,
+    onClose: onMilestoneModalClose,
+    onOpen: openMilestoneModal,
+  } = useDisclosure()
+
+  const handleMilestoneSubmit = (newMilestones: ProjectMilestone[]) => {
+    updateProject({ milestones: newMilestones } as Project)
+    onMilestoneModalClose()
+  }
 
   const renderMilestones = () => {
     if (project.milestones && project.milestones.length > 0) {
@@ -37,17 +55,46 @@ export const Milestones = () => {
   }
 
   return (
-    <CardLayout
-      id={ID.project.view.milestones}
-      flexDirection="column"
-      width="100%"
-      alignItems="flex-start"
-      spacing="25px"
-    >
-      <ProjectSectionBar name={'Milestones'} number={milestoneLength} />
-      <VStack alignItems="flex-start" spacing="12px">
-        {renderMilestones()}
-      </VStack>
-    </CardLayout>
+    <>
+      <CardLayout
+        id={ID.project.view.milestones}
+        flexDirection="column"
+        width="100%"
+        alignItems="flex-start"
+        spacing="25px"
+      >
+        <ProjectSectionBar
+          name={'Milestones'}
+          number={milestoneLength}
+          rightSection={
+            isProjectOwner && (
+              <IconButtonComponent
+                aria-label="edit-milestone"
+                noBorder
+                variant="ghost"
+                onClick={openMilestoneModal}
+                icon={<BiPencil fontSize="16px" />}
+              />
+            )
+          }
+        />
+        <VStack alignItems="flex-start" spacing="12px">
+          {renderMilestones()}
+        </VStack>
+      </CardLayout>
+      {isMilestoneModalOpen ? (
+        <MilestoneAdditionModal
+          isOpen={isMilestoneModalOpen}
+          onClose={onMilestoneModalClose}
+          availableMilestones={
+            project?.milestones && project.milestones.length > 0
+              ? (project.milestones as ProjectMilestone[])
+              : [defaultMilestone]
+          }
+          onSubmit={handleMilestoneSubmit}
+          projectId={parseInt(`${project.id}`, 10)}
+        />
+      ) : null}
+    </>
   )
 }
