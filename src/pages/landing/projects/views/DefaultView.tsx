51280016550acb39c306830c3b3ecbace3efd662
sajald77 <sazzaldulal@gmail.com>
2023-03-29 14:08:03 -0400

    fix: remove featured grant

--- src/pages/landing/projects/views/DefaultView.tsx
+++ src/pages/landing/projects/views/DefaultView.tsx
@@ -1,22 +1,15 @@
 import { CardLayout } from '../../../../components/layouts'
 import { getFeaturedProject, getListOfTags } from '../../../../constants'
-import { useGrants } from '../../../grants/hooks/useGrants'
 import {
   ProjectsDisplay,
   ProjectsDisplayMostFundedThisWeek,
 } from '../components'
 import { FeaturedProjectCard } from '../elements'
-import { FeaturedGrantCard } from '../elements'
 
 export const DefaultView = () => {
-  const { activeGrant, loading } = useGrants()
   return (
     <CardLayout w="full" spacing="50px" padding="20px">
-      {activeGrant ? (
-        <FeaturedGrantCard grant={activeGrant} loading={loading} />
-      ) : (
-        <FeaturedProjectCard projectName={getFeaturedProject()} />
-      )}
+      <FeaturedProjectCard projectName={getFeaturedProject()} />
 
       <ProjectsDisplay seeAllText="See all projects" />
       {getListOfTags().map((tag) => {
