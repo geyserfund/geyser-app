3c998d19dc7730600ed8ad94bc4ebe2c68998fe6
sajald77 <sazzaldulal@gmail.com>
2023-03-29 14:00:06 -0400

    fix: display featured project

--- src/pages/landing/projects/views/DefaultView.tsx
+++ src/pages/landing/projects/views/DefaultView.tsx
@@ -1,18 +1,23 @@
 import { CardLayout } from '../../../../components/layouts'
-import { getListOfTags } from '../../../../constants'
+import { getFeaturedProject, getListOfTags } from '../../../../constants'
 import { useGrants } from '../../../grants/hooks/useGrants'
 import {
   ProjectsDisplay,
   ProjectsDisplayMostFundedThisWeek,
 } from '../components'
+import { FeaturedProjectCard } from '../elements'
 import { FeaturedGrantCard } from '../elements'
 
 export const DefaultView = () => {
   const { activeGrant, loading } = useGrants()
   return (
     <CardLayout w="full" spacing="50px" padding="20px">
-      {/* <FeaturedProjectCard projectName={getFeaturedProject()} /> */}
-      <FeaturedGrantCard grant={activeGrant} loading={loading} />
+      {activeGrant ? (
+        <FeaturedGrantCard grant={activeGrant} loading={loading} />
+      ) : (
+        <FeaturedProjectCard projectName={getFeaturedProject()} />
+      )}
+
       <ProjectsDisplay seeAllText="See all projects" />
       {getListOfTags().map((tag) => {
         return <ProjectsDisplayMostFundedThisWeek key={tag.id} tag={tag} />
