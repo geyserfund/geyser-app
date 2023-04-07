4b80dc8db2d353bf58ea5bc1903c6a88eb19840d
sajald77 <sazzaldulal@gmail.com>
2023-04-07 14:36:25 -0400

    fix: minor refine

--- src/pages/grants/components/ListText.tsx
+++ src/pages/grants/components/ListText.tsx
@@ -29,7 +29,7 @@ export const ListText = ({
             <SatSymbolIcon {...satLogoProps} />
           </Box>
         ) : null}
-        {typeof children === 'string' ? (
+        {typeof children === 'string' || typeof children === 'number' ? (
           <Text
             fontWeight={'700'}
             fontSize={'19px'}
