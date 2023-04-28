3ce10eb7d0f1deb27a49b5e4dba63ba055a6dd06
sajald77 <sazzaldulal@gmail.com>
2023-04-28 13:36:16 -0400

    feat: add routing instrumentation for react router 6

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,34 @@ import { ColorModeScript } from '@chakra-ui/react'
 import * as Sentry from '@sentry/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from 'react-router-dom'
 
 import { App } from './App'
 import { GlobalStyles } from './config'
 
+const SENTRY_DSN =
+  'https://2355dca8304c4e32b35bf421d3cf4d87@o4504351883984896.ingest.sentry.io/4505088829292544'
+
 Sentry.init({
-  dsn: 'https://2355dca8304c4e32b35bf421d3cf4d87@o4504351883984896.ingest.sentry.io/4505088829292544',
-  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  dsn: SENTRY_DSN,
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Browser tracing integration creates a new transaction for each page load and navigation event, and creates a child span for every XMLHttpRequest or fetch request that occurs while those transactions are open.
+      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+        React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      ),
+    }),
+    new Sentry.Replay(),
+  ],
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
   // Session Replay
