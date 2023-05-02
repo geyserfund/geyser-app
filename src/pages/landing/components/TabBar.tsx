68ce4a15c8ef47a4220cc8736e014c5a7ee73dea
sajald77 <sazzaldulal@gmail.com>
2023-05-01 20:31:23 -0400

    fix: add profile avatar badge when activity is present

--- src/pages/landing/components/TabBar.tsx
+++ src/pages/landing/components/TabBar.tsx
@@ -1,10 +1,14 @@
-import { Avatar, ButtonProps } from '@chakra-ui/react'
+import { Avatar, AvatarBadge, Box, ButtonProps } from '@chakra-ui/react'
 import { useMatch, useNavigate } from 'react-router-dom'
 
 import { CardLayout, CardLayoutProps } from '../../../components/layouts'
 import { ButtonComponent } from '../../../components/ui'
 import { getPath } from '../../../constants'
-import { useAuthContext, useFilterContext } from '../../../context'
+import {
+  useActivitySubsciptionContext,
+  useAuthContext,
+  useFilterContext,
+} from '../../../context'
 
 type TabBarProps = CardLayoutProps
 
@@ -12,6 +16,7 @@ export const TabBar = (props: TabBarProps) => {
   const { clearFilter } = useFilterContext()
   const navigate = useNavigate()
   const { user } = useAuthContext()
+  const { activities } = useActivitySubsciptionContext()
 
   const isCurrentTabActivity = useMatch(getPath('landingFeed'))
 
@@ -45,11 +50,16 @@ export const TabBar = (props: TabBarProps) => {
         onClick={handleActivityClick}
       >
         {user.imageUrl ? (
-          <Avatar height="23px" width="23px" src={user.imageUrl} mr="10px" />
+          <Avatar height="23px" width="23px" src={user.imageUrl} mr="10px">
+            {activities.length > 0 && (
+              <AvatarBadge placement="top-end" color="primary.600" />
+            )}
+          </Avatar>
         ) : (
           ''
         )}
         Activity
+        <Box />
       </ButtonComponent>
     </CardLayout>
   )
