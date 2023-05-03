fef66e913e79ecf9572f9fd719e798d44df55d28
J Francisco Rader <franciscorader@gmail.com>
2023-05-03 00:19:10 -0300

    refactor: small code improvement + move function outside component

--- src/pages/landing/feed/views/ActivityList.tsx
+++ src/pages/landing/feed/views/ActivityList.tsx
@@ -7,52 +7,52 @@ import { ProjectActivityItem } from '../components/ProjectActivityItem'
 import { RewardActivityItem } from '../components/RewardActivityItem'
 import { ActivityResource } from '../types'
 
+const renderActivity = (activity: ActivityForLandingPageFragment) => {
+  if (!activity.resource) {
+    return null
+  }
+
+  switch (activity.resource.__typename) {
+    case ActivityResource.fundingTx:
+      return (
+        <ContributionActivityItem
+          fundingTx={activity.resource}
+          dateTime={activity.createdAt}
+          showsProjectLink
+        />
+      )
+    case ActivityResource.project:
+      return (
+        <ProjectActivityItem
+          project={activity.resource}
+          dateTime={activity.createdAt}
+        />
+      )
+    case ActivityResource.entry:
+      return (
+        <EntryActivityItem
+          entry={activity.resource}
+          dateTime={activity.createdAt}
+        />
+      )
+    case ActivityResource.projectReward:
+      return (
+        <RewardActivityItem
+          reward={activity.resource}
+          dateTime={activity.createdAt}
+        />
+      )
+
+    default:
+      return null
+  }
+}
+
 export const ActivityList = ({
   activities,
 }: {
   activities: ActivityForLandingPageFragment[]
 }) => {
-  const renderActivity = (activity: ActivityForLandingPageFragment) => {
-    if (!activity.resource) {
-      return null
-    }
-
-    switch (activity.resource.__typename) {
-      case ActivityResource.fundingTx:
-        return (
-          <ContributionActivityItem
-            fundingTx={activity.resource}
-            dateTime={activity.createdAt}
-            showsProjectLink
-          />
-        )
-      case ActivityResource.project:
-        return (
-          <ProjectActivityItem
-            project={activity.resource}
-            dateTime={activity.createdAt}
-          />
-        )
-      case ActivityResource.entry:
-        return (
-          <EntryActivityItem
-            entry={activity.resource}
-            dateTime={activity.createdAt}
-          />
-        )
-      case ActivityResource.projectReward:
-        return (
-          <RewardActivityItem
-            reward={activity.resource}
-            dateTime={activity.createdAt}
-          />
-        )
-
-      default:
-        return null
-    }
-  }
-
   if (activities.length === 0) {
     return <Text>This user has no activity yet.</Text>
   }
