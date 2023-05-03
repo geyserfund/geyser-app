39bb035c5224c341d61336565e79e19e626fdf60
J Francisco Rader <franciscorader@gmail.com>
2023-05-02 21:18:34 -0300

    chore: improve code readability + fix rewards cost

--- src/pages/projectDashboard/ProjectFundingSettings.tsx
+++ src/pages/projectDashboard/ProjectFundingSettings.tsx
@@ -45,124 +45,123 @@ export const ProjectFundingSettings = () => {
   const renderWalletConnectionDetails = () => {
     const { connectionDetails } = projectWallet || {}
 
-    const lndConnectionDetails =
-      connectionDetails as LndConnectionDetailsPrivate
-
-    const lightningConnectionDetails =
-      connectionDetails as LightningAddressConnectionDetails
-
     if (!connectionDetails) {
       return null
     }
 
-    switch (connectionDetails.__typename) {
-      case 'LightningAddressConnectionDetails':
-        return (
-          <GridItem colSpan={8} display="flex" justifyContent="center">
-            <ProjectFundingSettingsLightningAddressView
-              lightningAddress={lightningConnectionDetails.lightningAddress}
-            />
-          </GridItem>
-        )
-      default:
-        return (
-          <>
-            <VStack
-              width="100%"
-              border="1px solid"
-              borderColor={colors.gray300}
-              borderRadius="4px"
-              alignItems="flex-start"
-              padding="10px"
-              spacing="10px"
-            >
-              <HStack width="100%" justifyContent="space-between">
-                <Text fontWeight={500}>{projectWallet?.name}</Text>
-              </HStack>
-              <HStack width="100%">
-                <CheckCircleIcon color={colors.primary800} fontSize="12px" />
-                <Text color={colors.primary800} fontSize="12px">
-                  RUNNING
-                </Text>
-              </HStack>
+    if (connectionDetails.__typename === 'LightningAddressConnectionDetails') {
+      const lightningConnectionDetails =
+        connectionDetails as LightningAddressConnectionDetails
 
-              <VStack width="100%" spacing="4px" alignItems="flex-start">
-                <Text color="brand.neutral700" fontSize="10px">
-                  Hostname or IP address
-                </Text>
-                <Text
-                  wordBreak="break-all"
-                  color="brand.neutral900"
-                  fontSize="14px"
-                >
-                  {lndConnectionDetails.hostname}
-                </Text>
-              </VStack>
-
-              <VStack width="100%" spacing="4px" alignItems="flex-start">
-                <Text color="brand.neutral700" fontSize="10px">
-                  Public key
-                </Text>
-                <Text
-                  wordBreak="break-all"
-                  color="brand.neutral900"
-                  fontSize="14px"
-                >
-                  {lndConnectionDetails.pubkey}
-                </Text>
-              </VStack>
-              <VStack
-                width="100%"
-                spacing="4px"
-                alignItems="flex-start"
-                flexWrap="wrap"
-              >
-                <Text color="brand.neutral700" fontSize="10px">
-                  Invoice Macaroon
-                </Text>
-                <Text
-                  wordBreak="break-all"
-                  color="brand.neutral900"
-                  fontSize="14px"
-                >
-                  {lndConnectionDetails.macaroon}
-                </Text>
-              </VStack>
-              {lndConnectionDetails.tlsCertificate && (
-                <VStack width="100%" spacing="4px" alignItems="flex-start">
-                  <Text color="brand.neutral700" fontSize="10px">
-                    TLS certificate
-                  </Text>
-                  <Text
-                    wordBreak="break-all"
-                    color="brand.neutral900"
-                    fontSize="14px"
-                  >
-                    {lndConnectionDetails.tlsCertificate}
-                  </Text>
-                </VStack>
-              )}
-              <VStack width="100%" spacing="4px" alignItems="flex-start">
-                <Text color="brand.neutral700" fontSize="10px">
-                  gRPC port
-                </Text>
-                <Text
-                  wordBreak="break-all"
-                  color="brand.neutral900"
-                  fontSize="14px"
-                >
-                  {lndConnectionDetails.grpcPort}
-                </Text>
-              </VStack>
-            </VStack>
-            <Text color="brand.neutral700" fontSize="10px">
-              If you want to change how you receive your funds reach out to
-              hello@geyser.fund. We are not currently enabling editing of this
-              information for security reasons.
-            </Text>
-          </>
-        )
+      return (
+        <GridItem colSpan={8} display="flex" justifyContent="center">
+          <ProjectFundingSettingsLightningAddressView
+            lightningAddress={lightningConnectionDetails.lightningAddress}
+          />
+        </GridItem>
+      )
     }
+
+    const lndConnectionDetails =
+      connectionDetails as LndConnectionDetailsPrivate
+
+    return (
+      <>
+        <VStack
+          width="100%"
+          border="1px solid"
+          borderColor={colors.gray300}
+          borderRadius="4px"
+          alignItems="flex-start"
+          padding="10px"
+          spacing="10px"
+        >
+          <HStack width="100%" justifyContent="space-between">
+            <Text fontWeight={500}>{projectWallet?.name}</Text>
+          </HStack>
+          <HStack width="100%">
+            <CheckCircleIcon color={colors.primary800} fontSize="12px" />
+            <Text color={colors.primary800} fontSize="12px">
+              RUNNING
+            </Text>
+          </HStack>
+
+          <VStack width="100%" spacing="4px" alignItems="flex-start">
+            <Text color="brand.neutral700" fontSize="10px">
+              Hostname or IP address
+            </Text>
+            <Text
+              wordBreak="break-all"
+              color="brand.neutral900"
+              fontSize="14px"
+            >
+              {lndConnectionDetails.hostname}
+            </Text>
+          </VStack>
+
+          <VStack width="100%" spacing="4px" alignItems="flex-start">
+            <Text color="brand.neutral700" fontSize="10px">
+              Public key
+            </Text>
+            <Text
+              wordBreak="break-all"
+              color="brand.neutral900"
+              fontSize="14px"
+            >
+              {lndConnectionDetails.pubkey}
+            </Text>
+          </VStack>
+          <VStack
+            width="100%"
+            spacing="4px"
+            alignItems="flex-start"
+            flexWrap="wrap"
+          >
+            <Text color="brand.neutral700" fontSize="10px">
+              Invoice Macaroon
+            </Text>
+            <Text
+              wordBreak="break-all"
+              color="brand.neutral900"
+              fontSize="14px"
+            >
+              {lndConnectionDetails.macaroon}
+            </Text>
+          </VStack>
+          {lndConnectionDetails.tlsCertificate && (
+            <VStack width="100%" spacing="4px" alignItems="flex-start">
+              <Text color="brand.neutral700" fontSize="10px">
+                TLS certificate
+              </Text>
+              <Text
+                wordBreak="break-all"
+                color="brand.neutral900"
+                fontSize="14px"
+              >
+                {lndConnectionDetails.tlsCertificate}
+              </Text>
+            </VStack>
+          )}
+          <VStack width="100%" spacing="4px" alignItems="flex-start">
+            <Text color="brand.neutral700" fontSize="10px">
+              gRPC port
+            </Text>
+            <Text
+              wordBreak="break-all"
+              color="brand.neutral900"
+              fontSize="14px"
+            >
+              {lndConnectionDetails.grpcPort}
+            </Text>
+          </VStack>
+        </VStack>
+        <Text color="brand.neutral700" fontSize="10px">
+          If you want to change how you receive your funds reach out to
+          hello@geyser.fund. We are not currently enabling editing of this
+          information for security reasons.
+        </Text>
+      </>
+    )
   }
 
   return (
