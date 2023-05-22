9f36e8f5ff515914e176b245dba9e360ef9f6d98
J Francisco Rader <franciscorader@gmail.com>
2023-05-22 14:18:15 -0300

    fix: nostr refresh on login removed

--- src/graphql/fragments/user.ts
+++ src/graphql/fragments/user.ts
@@ -1,5 +1,31 @@
 import { gql } from '@apollo/client'
 
+export const FRAGMENT_USER_ME = gql`
+  fragment UserMe on User {
+    id
+    username
+    imageUrl
+    email
+    externalAccounts {
+      id
+      accountType
+      externalUsername
+      externalId
+      public
+    }
+    ownerOf {
+      project {
+        id
+        name
+        image
+        thumbnailImage
+        title
+        status
+      }
+    }
+  }
+`
+
 export const FRAGMENT_USER_FOR_AVATAR = gql`
   fragment UserForAvatar on User {
     id
