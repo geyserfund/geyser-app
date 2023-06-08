cdec9237c7506fcc9963f6777227f27821ca5612
J Francisco Rader <franciscorader@gmail.com>
2023-06-08 17:00:45 -0300

    chore: remove unused padding

--- src/pages/projectView/projectActivityPanel/screens/ProjectFundingRewardSelectionScreen.tsx
+++ src/pages/projectView/projectActivityPanel/screens/ProjectFundingRewardSelectionScreen.tsx
@@ -1,13 +1,10 @@
 import { VStack } from '@chakra-ui/react'
 
-import { useMobileMode } from '../../../../utils'
 import { FundingFormRewards } from '../components/FundingFormRewards'
 
 export const ProjectFundingRewardSelectionScreen = () => {
-  const isMobile = useMobileMode()
   return (
     <VStack
-      padding={isMobile ? '20px 10px' : '20px'}
       py={4}
       px={{ base: 2, lg: 4 }}
       width="100%"
