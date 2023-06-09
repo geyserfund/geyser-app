66eb88300b9ebb8666e0b0c6fbe1d21def0d1f53
J Francisco Rader <franciscorader@gmail.com>
2023-06-09 13:25:25 -0300

    fix: objective textarea fix + tooltip placement

--- src/pages/projectCreate/components/ProjectForm.tsx
+++ src/pages/projectCreate/components/ProjectForm.tsx
@@ -151,7 +151,7 @@ export const ProjectForm = ({ form, isEdit }: ProjectFormProps) => {
               target: {
                 ...target,
                 name: target.name,
-                value: target.value.replace(/\s/gm, ''),
+                value: target.value.replace(/\n/gm, ''),
               },
               ...event,
             })
