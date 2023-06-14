c345cb2516f18f3b3d10092f418ad75bfce0e0fe
Fran <franciscorader@gmail.com>
2023-06-14 19:33:32 -0300

    fix: project entry card follow and sats icon fix (#963)
    * fix: project entry card follow and sats icon fix
    
    * chore: remove unused import

--- src/components/molecules/projectDisplay/ProjectEntryCard.tsx
+++ src/components/molecules/projectDisplay/ProjectEntryCard.tsx
@@ -1,5 +1,5 @@
 import { CloseIcon } from '@chakra-ui/icons'
-import { Box, HStack, Image, Spacer, Stack } from '@chakra-ui/react'
+import { Box, HStack, Image, Spacer, Stack, Text } from '@chakra-ui/react'
 import { DateTime } from 'luxon'
 import { useMemo } from 'react'
 import { BiPencil } from 'react-icons/bi'
@@ -15,7 +15,7 @@ import {
 } from '../../../types/generated/graphql'
 import { getShortAmountLabel, toInt } from '../../../utils'
 import { CardLayout } from '../../layouts'
-import { Body1, Body2, H2, MonoBody1 } from '../../typography'
+import { Body1, Body2, H2 } from '../../typography'
 import { ICard, IconButtonComponent, SatoshiAmount } from '../../ui'
 import { EntryStatusLabel } from '../../ui/EntryStatusLabel'
 import { ProjectEntryCardThumbnailPlaceholder } from './ProjectEntryCardThumbnailPlaceholder'
@@ -163,18 +163,22 @@ export const ProjectEntryCard = ({ entry, onEdit, onDelete }: Props) => {
           spacing={{ base: '10px', lg: '22px' }}
           overflow="hidden"
         >
-          <HStack spacing={'10px'} align={'center'} flex={0}>
+          <HStack
+            color="primary.400"
+            spacing={'10px'}
+            align={'center'}
+            flex={0}
+          >
             <HStack spacing={1}>
-              <MonoBody1 color="primary.400" fontWeight={'bold'}>
+              <Text fontFamily={fonts.mono} fontWeight="bold" variant="body1">
                 {entry.fundersCount}
-              </MonoBody1>
-              <BsHeartFill color={'primary.400'} />
+              </Text>
+              <BsHeartFill />
             </HStack>
 
             <SatoshiAmount
               fontFamily={fonts.mono}
-              color="primary.400"
-              fontSize="16px"
+              variant="body1"
               fontWeight="bold"
               scale={0.8}
             >
