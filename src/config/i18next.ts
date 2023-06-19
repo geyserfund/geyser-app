5ef59dd947f2df2bb4b04054d91caa772e39d1d4
sajald77 <sazzaldulal@gmail.com>
2023-06-19 12:55:40 -0400

    fix: missing change

--- src/config/i18next.ts
+++ src/config/i18next.ts
@@ -2,6 +2,7 @@ import i18next from 'i18next'
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
+import { lng } from '../constants'
 import { EnglishTranslations, SpanishTranslations } from '../translations'
 
 i18next
