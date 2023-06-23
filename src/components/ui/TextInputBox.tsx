12b93c7e8c3ee6f470db3e73e41f222850c4416a
Fran <franciscorader@gmail.com>
2023-06-23 10:20:36 -0300

    feat: hide keyboard on search + improve filters dependencies (#978)

--- src/components/ui/TextInputBox.tsx
+++ src/components/ui/TextInputBox.tsx
@@ -7,6 +7,7 @@ import {
   InputRightElement,
   Text,
 } from '@chakra-ui/react'
+import { forwardRef } from 'react'
 
 interface TextInputBoxProps extends InputProps {
   error?: React.ReactNode
@@ -14,51 +15,48 @@ interface TextInputBoxProps extends InputProps {
   rightIcon?: React.ReactNode
 }
 
-export const TextInputBox = ({
-  children,
-  error,
-  leftIcon,
-  rightIcon,
-  ...rest
-}: TextInputBoxProps) => {
-  return (
-    <Box width="100%">
-      <InputGroup>
-        {leftIcon && (
-          <InputLeftElement pointerEvents="none">{leftIcon}</InputLeftElement>
-        )}
-        <Input
-          isInvalid={Boolean(error)}
-          borderRadius="8px"
-          borderWidth="2px"
-          borderColor="neutral.200"
-          backgroundColor="neutral.0"
-          _hover={{
-            borderColor: 'neutral.400',
-          }}
-          _focus={{ borderColor: `neutral.500`, boxShadow: 'none' }}
-          _placeholder={{
-            color: 'neutral.600',
-          }}
-          {...rest}
-        >
-          {children}
-        </Input>
-        {rightIcon && (
-          <InputRightElement pointerEvents="none">
-            {rightIcon}
-          </InputRightElement>
-        )}
-      </InputGroup>
-      {error ? (
-        typeof error === 'object' ? (
-          error
-        ) : (
-          <Text color="secondary.red" fontSize="12px">
-            {error}
-          </Text>
-        )
-      ) : null}
-    </Box>
-  )
-}
+export const TextInputBox = forwardRef<HTMLInputElement, TextInputBoxProps>(
+  ({ children, error, leftIcon, rightIcon, ...rest }, ref) => {
+    return (
+      <Box width="100%">
+        <InputGroup>
+          {leftIcon && (
+            <InputLeftElement pointerEvents="none">{leftIcon}</InputLeftElement>
+          )}
+          <Input
+            ref={ref}
+            isInvalid={Boolean(error)}
+            borderRadius="8px"
+            borderWidth="2px"
+            borderColor="neutral.200"
+            backgroundColor="neutral.0"
+            _hover={{
+              borderColor: 'neutral.400',
+            }}
+            _focus={{ borderColor: `neutral.500`, boxShadow: 'none' }}
+            _placeholder={{
+              color: 'neutral.600',
+            }}
+            {...rest}
+          >
+            {children}
+          </Input>
+          {rightIcon && (
+            <InputRightElement pointerEvents="none">
+              {rightIcon}
+            </InputRightElement>
+          )}
+        </InputGroup>
+        {error ? (
+          typeof error === 'object' ? (
+            error
+          ) : (
+            <Text color="secondary.red" fontSize="12px">
+              {error}
+            </Text>
+          )
+        ) : null}
+      </Box>
+    )
+  },
+)
