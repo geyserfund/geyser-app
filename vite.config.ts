e5ee5167da020c9f28a88389e58a964c73719fc7
sajald77 <sazzaldulal@gmail.com>
2023-08-06 18:17:40 -0400

    fix: updates to PWA for app installabitlity

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,36 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig, loadEnv, PluginOption } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  registerType: 'autoUpdate',
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+  },
+  includeAssets: ['logo-brand.svg'],
+  manifest: {
+    start_url: '.',
+    display: 'standalone',
+    background_color: '#ffffff',
+    name: 'Geyser Fund - Crowdfunding with Bitcoin',
+    short_name: 'Geyser',
+    description:
+      'Geyser is a bitcoin crowdfunding platform that enables campaign creators to launch their projects with rewards and engage their communities with posts and content.',
+    theme_color: '#20ECC7',
+    icons: [
+      {
+        src: 'logo-brand.svg',
+        sizes:
+          '36x36 48x48 57x57 60x60 72x72 76x76 96x96 114x114 120x120 128x128 144x144 152x152 180x180 192x192 256x256 384x384 512x512',
+        type: 'image/svg+xml',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+}
 
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -28,34 +57,7 @@ export default defineConfig(({ command, mode }) => {
       `)
   }
 
-  const plugins: PluginOption[] = [
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-      includeAssets: ['logo-brand.svg'],
-      manifest: {
-        start_url: '.',
-        display: 'standalone',
-        background_color: '#ffffff',
-        name: 'Geyser Fund - Crowdfunding with Bitcoin',
-        short_name: 'Geyser',
-        description:
-          'Geyser is a bitcoin crowdfunding platform that enables campaign creators to launch their projects with rewards and engage their communities with posts and content.',
-        theme_color: '#20ECC7',
-        icons: [
-          {
-            src: 'logo-brand.svg',
-            sizes:
-              '36x36 48x48 57x57 60x60 72x72 76x76 96x96 114x114 120x120 128x128 144x144 152x152 180x180 192x192 256x256 384x384 512x512',
-            type: 'image/svg+xml',
-          },
-        ],
-      },
-    }),
-    react(),
-  ]
+  const plugins: PluginOption[] = [VitePWA({}), react()]
   if (mode === 'development') {
     plugins.push(mkcert())
   }
