03f9afcf59508b56e3d38d6c5b7fd14f5e446810
sajald77 <sazzaldulal@gmail.com>
2023-08-06 18:40:09 -0400

    fix: add pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -23,10 +23,33 @@ const pwaOptions: Partial<VitePWAOptions> = {
     theme_color: '#20ECC7',
     icons: [
       {
-        src: 'logo-brand.svg',
-        sizes:
-          '36x36 48x48 57x57 60x60 72x72 76x76 96x96 114x114 120x120 128x128 144x144 152x152 180x180 192x192 256x256 384x384 512x512',
-        type: 'image/svg+xml',
+        src: '/icons/16.png',
+        sizes: '16x16',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+      {
+        src: '/icons/32.png',
+        sizes: '32x32',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+      {
+        src: '/icons/180.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+      {
+        src: '/icons/192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+      {
+        src: '/icons/512.png',
+        sizes: '512x512',
+        type: 'image/png',
         purpose: 'any maskable',
       },
     ],
