988a1093ae3efebf52f1f0e02cc732ad1fea7742
sajald77 <sazzaldulal@gmail.com>
2023-08-11 16:45:13 -0400

    fix:  a plus icon next to the project create icon

--- src/pages/profile/components/CreateAProjectButton.tsx
+++ src/pages/profile/components/CreateAProjectButton.tsx
@@ -1,21 +1,23 @@
-import { ButtonProps } from '@chakra-ui/react'
+import { AddIcon } from '@chakra-ui/icons'
+import { Button, ButtonProps } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
-import { ButtonComponent } from '../../../components/ui'
 import { getPath } from '../../../constants'
 
 export const CreateAProjectButton = (props: ButtonProps) => {
   const { t } = useTranslation()
   return (
-    <ButtonComponent
-      marginTop="20px "
+    <Button
+      variant="primary"
+      marginTop="20px"
+      px="10px"
       as={Link}
       to={getPath('publicProjectLaunch')}
-      primary
       {...props}
+      leftIcon={<AddIcon fontSize={'12px'} />}
     >
       {t('Create a project')}
-    </ButtonComponent>
+    </Button>
   )
 }
