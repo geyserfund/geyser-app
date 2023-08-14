6e3bb1188c1de8ea0286634222d7ae3becfe39ae
sajald77 <sazzaldulal@gmail.com>
2023-08-14 16:22:23 -0400

    fix: replace copies

--- src/pages/projectView/projectNavigation/components/ProjectCreatorModal.tsx
+++ src/pages/projectView/projectNavigation/components/ProjectCreatorModal.tsx
@@ -33,7 +33,7 @@ export const ProjectCreatorModal = (props: UseModalReturn) => {
         />
         <CreationMenuItem
           icon={<RewardGiftIcon />}
-          title={t('Sell reward items')}
+          title={t('Sell a reward item')}
           description={t(
             'Sell your work, merch, art, sponsorships, badges, or services',
           )}
