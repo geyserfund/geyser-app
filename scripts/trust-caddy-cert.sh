0c082d5722e8c51b0476e373cbcb5284fc654424
Stelios Rammos <stelio.rammos@gmail.com>
2023-08-28 12:42:29 -0500

    feat: add scripts for trusting caddy local CA

---
+++ scripts/trust-caddy-cert.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if security find-certificate -c "Caddy Local Authority" -p >> /dev/null; then
+    echo 'Caddy Local Authority is trusted.'
+else
+    echo 'Adding Caddy Local Authority to KeyChain.'
+    CERT_FILE=../caddy/certificate/root.crt
+    docker exec geyser-caddy-standalone cat /data/caddy/pki/authorities/local/root.crt > $CERT_FILE
+    security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $CERT_FILE
+fi;
