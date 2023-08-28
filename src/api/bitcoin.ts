465b9ad4414e7ffcf339ccf80c93c5ca57c75cba
sajald77 <sazzaldulal@gmail.com>
2023-08-28 09:37:15 -0400

    fix: update fetch-api to handle failed cases with localstorage and backup

--- src/api/bitcoin.ts
+++ src/api/bitcoin.ts
@@ -1,16 +1,15 @@
-import { AxiosResponse } from 'axios'
-import axios from 'axios'
-
+const USD_QUOTE_KEY = 'usdQuote'
+const BACKUP_QUOTE = 26088
 const quoteSources = [
+  {
+    name: 'blockchain.info',
+    url: 'https://blockchain.info/ticker',
+    lastPrice: (response: any) => Number(response.USD.last),
+  },
   {
     name: 'bitstamp',
     url: 'https://www.bitstamp.net/api/v2/ticker/btcusd/',
-    lastPrice: (response: AxiosResponse) => Number(response.data.last),
-  },
-  {
-    name: 'bitfinex',
-    url: 'https://api-pub.bitfinex.com/v2/ticker/tBTCUSD',
-    lastPrice: (response: AxiosResponse) => Number(response.data[6]),
+    lastPrice: (response: any) => Number(response.data.last),
   },
 ]
 
@@ -18,16 +17,25 @@ const getUsdQuote = async (): Promise<number> => {
   const requests = quoteSources.map(
     ({ url, lastPrice }) =>
       new Promise((resolve, reject) => {
-        axios
-          .get(url)
-          .then((response: AxiosResponse) => resolve(lastPrice(response)))
+        fetch(url)
+          .then((response: Response) => response.json())
+          .then((response: any) => resolve(lastPrice(response)))
           .catch((error: Error) => reject(error))
       }),
   )
 
-  const usdQuote = (await Promise.any(requests)) as number
+  let usdQuote = Number(await Promise.any(requests))
 
-  if (!usdQuote) throw new Error('Could not get bitcoin/usd quote')
+  if (!usdQuote) {
+    usdQuote = Number(localStorage.getItem(USD_QUOTE_KEY)) || 0
+  }
+
+  if (!usdQuote) {
+    alert('Failed to fetch bitcoin rates')
+    usdQuote = BACKUP_QUOTE
+  }
+
+  localStorage.setItem(USD_QUOTE_KEY, String(usdQuote))
 
   return usdQuote
 }
