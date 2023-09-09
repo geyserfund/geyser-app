8dd7747cc017f31498ec1da5f5250ed23d9bd2a7
sajald77 <sazzaldulal@gmail.com>
2023-09-09 17:51:44 -0400

    fix: update text color

--- src/components/ui/PullingDownContent.tsx
+++ src/components/ui/PullingDownContent.tsx
@@ -2,21 +2,22 @@ import { HStack, Text } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { FiChevronsDown } from 'react-icons/fi'
 
-import { lightModeColors } from '../../styles'
+import { useCustomTheme } from '../../utils'
 
 export const PullingDownContent = (props: any) => {
   const { t } = useTranslation()
+  const { colors } = useCustomTheme()
 
   const renderNArrows = (n: number) => {
     return [...Array(n).keys()].map((_, i) => (
-      <FiChevronsDown color={lightModeColors.primary[400]} key={i} />
+      <FiChevronsDown color={colors.neutral[1000]} key={i} />
     ))
   }
 
   return (
     <HStack pt="5px" spacing="10px" w="full" justifyContent="center">
       {renderNArrows(3)}
-      <Text color="primary.400">{t('Pulldown to refresh')}</Text>
+      <Text color="neutral.1000">{t('Pulldown to refresh')}</Text>
       {renderNArrows(3)}
     </HStack>
   )
