1ff10e3e26d22481acbf9cb47fd4fe40afc28d1c
sajald77 <sazzaldulal@gmail.com>
2023-09-14 13:38:52 -0400

    fix: trim leading/trailing spaces

--- src/utils/validations/yup.ts
+++ src/utils/validations/yup.ts
@@ -3,6 +3,7 @@ import * as yup from 'yup'
 export const emailValidationSchema = yup.object({
   email: yup
     .string()
+    .trim()
     .required('Email is a required field')
     .email('Please enter a valid Email address'),
 })
