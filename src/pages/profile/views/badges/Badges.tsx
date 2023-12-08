8ab51d2cdf151c428d6ec23239146ecb9ff0d2ff
turizspace <143194249+turizspace@users.noreply.github.com>
2023-12-08 15:33:16 +0300

    Update Badges.tsx

--- src/pages/profile/views/badges/Badges.tsx
+++ src/pages/profile/views/badges/Badges.tsx
@@ -1,49 +1,40 @@
-import { Button, HStack, Link as ChakraLink } from '@chakra-ui/react'
-import { useEffect } from 'react'
-import { useTranslation } from 'react-i18next'
-import { BsBoxArrowUpRight } from 'react-icons/bs'
+import { Button, HStack, Link as ChakraLink, Box } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { BsBoxArrowUpRight } from 'react-icons/bs';
 
-import { CardLayout } from '../../../../components/layouts'
-import { Body2, H2 } from '../../../../components/typography'
-import { getPath } from '../../../../constants'
-import { User, useUserBadgesLazyQuery } from '../../../../types'
-import { toInt, useNotification } from '../../../../utils'
-import { BadgesBody, BadgesBodySkeleton } from './BadgesBody'
+import { CardLayout } from '../../../../components/layouts';
+import { Body2, H2 } from '../../../../components/typography';
+import { getPath } from '../../../../constants';
+import { User, useUserBadgesLazyQuery } from '../../../../types';
+import { toInt, useNotification } from '../../../../utils';
+import { BadgesBody, BadgesBodySkeleton } from './BadgesBody';
 
-export const Badges = ({
-  userProfile,
-  isEdit,
-  isLoading,
-}: {
-  userProfile: User
-  isEdit: boolean
-  isLoading: boolean
-}) => {
-  const { t } = useTranslation()
-  const { toast } = useNotification()
+export const Badges = ({ userProfile, isEdit, isLoading }: { userProfile: User; isEdit: boolean; isLoading: boolean }) => {
+  const { t } = useTranslation();
+  const { toast } = useNotification();
 
-  const [getUserBadges, { data: userBadgesData, loading: userBadgeLoading }] =
-    useUserBadgesLazyQuery({
-      onError() {
-        toast({
-          title: 'Error fetching badges',
-          description: 'Please refresh the page and try again.',
-          status: 'error',
-        })
-      },
-    })
+  const [getUserBadges, { data: userBadgesData, loading: userBadgeLoading }] = useUserBadgesLazyQuery({
+    onError() {
+      toast({
+        title: 'Error fetching badges',
+        description: 'Please refresh the page and try again.',
+        status: 'error',
+      });
+    },
+  });
 
   useEffect(() => {
     if (userProfile.id) {
       getUserBadges({
         variables: { input: { where: { userId: toInt(userProfile.id) } } },
-      })
+      });
     }
-  }, [userProfile.id, getUserBadges])
+  }, [userProfile.id, getUserBadges]);
 
   const renderBadgesBody = () => {
     if (userBadgeLoading || isLoading) {
-      return <BadgesBodySkeleton />
+      return <BadgesBodySkeleton />;
     }
 
     return (
@@ -54,40 +45,31 @@ export const Badges = ({
           isEdit,
         }}
       />
-    )
-  }
+    );
+  };
 
   return (
-    <CardLayout
-      noMobileBorder
-      padding="0"
-      paddingY={{ base: '10px', lg: '0px' }}
-      spacing="20px"
-      style={{ position: 'relative', maxHeight: '320px', overflowY: 'auto' }}
-    >
-      <HStack
-        paddingX={{ base: '10px', lg: '20px' }}
-        w="full"
-        justifyContent="space-between"
-        style={{ padding: '10px 20px', margin: 0, position: 'sticky', top: 0, backgroundColor: '#FFFFFF', zIndex: 1 }}
-      >
-        <H2>{t('Badges')}</H2>
-        <Button
-          as={ChakraLink}
-          href={getPath('badges')}
-          isExternal
-          size="sm"
-          leftIcon={<BsBoxArrowUpRight />}
-        >
-          {t('See badges')}
-        </Button>
-      </HStack>
-      <Body2 paddingX={{ base: '10px', lg: '20px' }} color="neutral.700">
-        {t(
-          'Geyser badges are earned for launching successful projects, contributing  to them and being an active community member.',
-        )}
-      </Body2>
-      {renderBadgesBody()}
-    </CardLayout>
-  )
-}
+    <Box as="section" paddingY={{ base: '10px', lg: '20px' }}>
+      <CardLayout noMobileBorder padding="0" spacing="20px">
+        <HStack paddingX={{ base: '10px', lg: '20px' }} w="full" justifyContent="space-between">
+          <H2>{t('Badges')}</H2>
+          <Button
+            as={ChakraLink}
+            href={getPath('badges')}
+            isExternal
+            size="sm"
+            leftIcon={<BsBoxArrowUpRight />}
+          >
+            {t('See badges')}
+          </Button>
+        </HStack>
+        <Body2 paddingX={{ base: '10px', lg: '20px' }} color="neutral.700">
+          {t(
+            'Geyser badges are earned for launching successful projects, contributing  to them and being an active community member.',
+          )}
+        </Body2>
+        {renderBadgesBody()}
+      </CardLayout>
+    </Box>
+  );
+};
