65f5aefbd4e73f7f47162050e2641ebbc02225a9
turizspace <143194249+turizspace@users.noreply.github.com>
2024-01-05 05:37:23 +0300

    fixes Noteslist.tsx
    fixed pagination. loading 12 projects. and load more button loading 10 more.
    border around projects added.

--- src/pages/landing/projects/components/Noteslist.tsx
+++ src/pages/landing/projects/components/Noteslist.tsx
@@ -1,4 +1,5 @@
-import { Event } from "nostr-tools";
+import { useState } from 'react';
+import { Event } from 'nostr-tools';
 import NoteCard from './NoteCard.tsx';
 
 interface Props {
@@ -6,16 +7,40 @@ interface Props {
 }
 
 export default function Noteslist({ projects }: Props) {
-  const sortedProjects = projects.slice().sort((a, b) => {
-    // Sorting logic here if needed
-  });
+  const initialDisplayCount = 12;
+  const itemsPerPage = 10; 
+  const [displayedProjects, setDisplayedProjects] = useState<Event[]>([]);
+  const [displayCount, setDisplayCount] = useState(initialDisplayCount);
+
+  const loadMore = () => {
+    const nextDisplayCount = displayCount + itemsPerPage;
+    setDisplayCount(nextDisplayCount);
+  };
+
+  const initialProjects = projects.slice(0, initialDisplayCount);
+
+  const renderProjects =
+    displayCount > initialDisplayCount ? projects.slice(0, displayCount) : initialProjects;
+
+  const buttonStyle = {
+    border: '1px solid #ccc',
+    padding: '8px 16px',
+    borderRadius: '4px',
+    margin: '8px 0',
+    cursor: 'pointer',
+  };
 
   return (
     <div style={{ margin: '0 20px', padding: '10px' }}>
       <div className="flex" style={{ display: 'flex', flexDirection: 'column' }}>
-        {sortedProjects.map((nostr, index) => (
+        {renderProjects.map((nostr, index) => (
           <NoteCard key={index} nostrich={nostr.pubkey} content={nostr.content} />
         ))}
+        {projects.length > displayCount && (
+          <button style={buttonStyle} onClick={loadMore}>
+            Load More
+          </button>
+        )}
       </div>
     </div>
   );
