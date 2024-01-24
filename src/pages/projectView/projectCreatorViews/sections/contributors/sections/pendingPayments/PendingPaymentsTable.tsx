fd09057cfaa6e5c6e415e49d008b8a3c47144004
sajald77 <sazzaldulal@gmail.com>
2024-01-24 13:35:43 -0500

    fix: add missing user external account info

--- src/pages/projectView/projectCreatorViews/sections/contributors/sections/pendingPayments/PendingPaymentsTable.tsx
+++ src/pages/projectView/projectCreatorViews/sections/contributors/sections/pendingPayments/PendingPaymentsTable.tsx
@@ -1,18 +1,24 @@
-import { HStack, VStack } from '@chakra-ui/react'
+import { EmailIcon } from '@chakra-ui/icons'
+import { HStack, IconButton, Link, VStack } from '@chakra-ui/react'
 import { DateTime } from 'luxon'
 import { useMemo } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
+import { BsFacebook } from 'react-icons/bs'
+import { RiTwitterXLine } from 'react-icons/ri'
 
+import { NostrSvgIcon } from '../../../../../../../components/icons'
 import { Body2 } from '../../../../../../../components/typography'
 import {
   AnonymousAvatar,
   LinkableAvatar,
 } from '../../../../../../../components/ui'
 import {
+  ExternalAccount,
   FundingConfirmInput,
   FundingTxOrderFragment,
 } from '../../../../../../../types'
-import { getUSD, OrderAmounts, OrderItems } from '../../components'
+import { ExternalAccountType } from '../../../../../../auth'
+import { getUSD, OrderItems } from '../../components'
 import { AccordionListItem } from '../../components/AccordionListItem'
 import {
   TableData,
@@ -116,6 +122,10 @@ export const PendingPaymentsTable = ({
         isAccordion: true,
         render(fundingTx: FundingTxOrderFragment) {
           const amount = fundingTx.amount - fundingTx.donationAmount
+          if (amount === 0) {
+            return null
+          }
+
           const items = [
             ...(fundingTx.bitcoinQuote?.quote
               ? [
@@ -139,6 +149,10 @@ export const PendingPaymentsTable = ({
         key: 'donation',
         isAccordion: true,
         render(fundingTx: FundingTxOrderFragment) {
+          if (fundingTx.donationAmount === 0) {
+            return null
+          }
+
           const items = [
             ...(fundingTx.bitcoinQuote?.quote
               ? [
@@ -165,26 +179,59 @@ export const PendingPaymentsTable = ({
         key: 'total',
         isAccordion: true,
         render(fundingTx: FundingTxOrderFragment) {
-          return (
-            <OrderAmounts
-              amount={fundingTx.amount}
-              quote={fundingTx.bitcoinQuote?.quote}
-            />
-          )
+          const items = [
+            ...(fundingTx.bitcoinQuote?.quote
+              ? [
+                  {
+                    label: t('Invoice total'),
+                    value: getUSD(
+                      fundingTx.amount,
+                      fundingTx.bitcoinQuote?.quote,
+                    ),
+                  },
+                ]
+              : []),
+            {
+              label: t('Invoice total (Sats)'),
+              value: fundingTx.amount,
+            },
+            {
+              label: t('Amount paid (Sats)'),
+              value: fundingTx.amountPaid,
+            },
+            ...(fundingTx.bitcoinQuote?.quote
+              ? [
+                  {
+                    label: t('Bitcoin Price'),
+                    value: `$${fundingTx.bitcoinQuote?.quote}`,
+                  },
+                ]
+              : []),
+          ]
+
+          return <AccordionListItem items={items} />
         },
       },
     ],
     [t, handleUpdate],
   )
 
-  const accordionContent = (item: FundingTxOrderFragment) => {
+  const accordionContent = (fundingTx: FundingTxOrderFragment) => {
+    const amount = fundingTx.amount - fundingTx.amountPaid
+
+    const amountInDollars = fundingTx.bitcoinQuote?.quote
+      ? getUSD(amount, fundingTx.bitcoinQuote?.quote).replace('<', 'less than')
+      : ''
+    const externalAccounts = fundingTx.funder.user?.externalAccounts || []
+
+    console.log('checking userinfop', fundingTx.funder.user)
     return (
       <HStack w="full" flex={1}>
-        <VStack alignItems="flex-start">
+        <VStack alignItems="flex-start" maxWidth="400px">
           <Body2>
             <Trans
               i18nKey={'Reward purchase is missing {{MISSING_AMOUNT}}'}
-              values={{ MISSING_AMOUNT: '$34.32 (20,000 Sats)' }}
+              values={{ MISSING_AMOUNT: `${amountInDollars} (${amount} Sats)` }}
             >{`Reward purchase is missing {{MISSING_AMOUNT}}`}</Trans>
           </Body2>
           <Body2>
@@ -192,6 +239,40 @@ export const PendingPaymentsTable = ({
               'Please contact the purchaser using the following methods, and Accept the purchase once the missing payments have been sent:',
             )}
           </Body2>
+          <HStack>
+            {externalAccounts?.map((account) => {
+              const values =
+                externalAccountIconAndColors[
+                  account.accountType as keyof typeof externalAccountIconAndColors
+                ]
+              if (!values) return null
+              return (
+                <IconButton
+                  key={account.id}
+                  size="sm"
+                  aria-label={account.accountType}
+                  icon={<values.icon color="white" maxHeight="20px" />}
+                  backgroundColor={values.color}
+                  _hover={{ backgroundColor: 'neutral.500' }}
+                  px="15px"
+                  as={Link}
+                  isExternal
+                  href={values.link(account)}
+                />
+              )
+            })}
+            <IconButton
+              aria-label="Email"
+              size="sm"
+              px="15px"
+              icon={<EmailIcon color="neutral.0" />}
+              backgroundColor="neutral.700"
+              _hover={{ backgroundColor: 'neutral.500' }}
+              onClick={() => {
+                window.open(`mailto:${fundingTx.email}`)
+              }}
+            />
+          </HStack>
         </VStack>
       </HStack>
     )
@@ -204,4 +285,25 @@ export const PendingPaymentsTable = ({
       accordionContent={accordionContent}
     />
   )
+}
+
+export const externalAccountIconAndColors = {
+  [ExternalAccountType.facebook]: {
+    icon: BsFacebook,
+    color: 'social.facebook',
+    link: (account: ExternalAccount) =>
+      `https://facebook.com/${account.externalUsername}`,
+  },
+  [ExternalAccountType.twitter]: {
+    icon: RiTwitterXLine,
+    color: 'social.twitter',
+    link: (account: ExternalAccount) =>
+      `https://twitter.com/${account.externalUsername}`,
+  },
+  [ExternalAccountType.nostr]: {
+    icon: NostrSvgIcon,
+    color: 'social.nostr',
+    link: (account: ExternalAccount) =>
+      `https://primal.net/profile/${account.externalId}`,
+  },
 }
