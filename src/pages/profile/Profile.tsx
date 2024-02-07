0f22bf7734a53ca5f5102dfa928474ccbd0400e9
sajald77 <sazzaldulal@gmail.com>
2024-02-06 20:16:35 -0500

    chore: cleanup

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,12 +1,6 @@
 import { useQuery } from '@apollo/client'
-import {
-  Center,
-  Container,
-  GridItem,
-  SimpleGrid,
-  VStack,
-} from '@chakra-ui/react'
-import { useEffect, useMemo, useState } from 'react'
+import { Center, Container, GridItem, SimpleGrid, VStack } from '@chakra-ui/react'
+import { useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 
@@ -39,15 +33,9 @@ export const Profile = () => {
 
   const [userProfile, setUserProfile] = useState<User>({ ...defaultUser })
 
-  const isViewingOwnProfile = useMemo(
-    () => params.userId === currentAppUser.id,
-    [params.userId, currentAppUser.id],
-  )
+  const isViewingOwnProfile = useMemo(() => params.userId === currentAppUser.id, [params.userId, currentAppUser.id])
 
-  const { loading: profileLoading, error } = useQuery<
-    ResponseData,
-    QueryVariables
-  >(QUERY_USER_PROFILE, {
+  const { loading: profileLoading, error } = useQuery<ResponseData, QueryVariables>(QUERY_USER_PROFILE, {
     variables: {
       where: {
         id,
@@ -71,23 +59,13 @@ export const Profile = () => {
 
   if (error) {
     return (
-      <Container
-        position="relative"
-        height="100%"
-        display={'flex'}
-        justifyContent="center"
-        alignItems="center"
-      >
+      <Container position="relative" height="100%" display={'flex'} justifyContent="center" alignItems="center">
         <Center>
           <AlertBox
             height="200px"
             status="error"
-            title={t(
-              'An error occurred while attempting to load the profile page.',
-            )}
-            message={t(
-              'Please try refreshing the page. You may also want to contact support if the problem persists.',
-            )}
+            title={t('An error occurred while attempting to load the profile page.')}
+            message={t('Please try refreshing the page. You may also want to contact support if the problem persists.')}
           />
         </Center>
       </Container>
@@ -126,11 +104,7 @@ export const Profile = () => {
           colSpan={{ base: 1, lg: 3 }}
           order={{ base: 3, lg: 2 }}
         >
-          <ProfileTabs
-            userProfile={userProfile}
-            isLoading={profileLoading}
-            isViewingOwnProfile={isViewingOwnProfile}
-          />
+          <ProfileTabs userProfile={userProfile} isLoading={profileLoading} isViewingOwnProfile={isViewingOwnProfile} />
         </GridItem>
         <GridItem
           h="100%"
@@ -140,11 +114,7 @@ export const Profile = () => {
         >
           <VStack spacing="10px">
             <Summary userProfile={userProfile} />
-            <Badges
-              userProfile={userProfile}
-              isEdit={isViewingOwnProfile}
-              isLoading={profileLoading}
-            />
+            <Badges userProfile={userProfile} isEdit={isViewingOwnProfile} isLoading={profileLoading} />
           </VStack>
         </GridItem>
       </SimpleGrid>
