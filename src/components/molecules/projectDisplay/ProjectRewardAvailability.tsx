8af0a0eda9c1259b79541674f1a4d08b0a7840f8
Travis Hoglund <travis@travishoglund.com>
2024-02-13 17:10:02 -0600

    fix: fixes needed

--- src/components/molecules/projectDisplay/ProjectRewardAvailability.tsx
+++ src/components/molecules/projectDisplay/ProjectRewardAvailability.tsx
@@ -12,11 +12,8 @@ export const ProjectRewardAvailability = ({ numberOfRewardsAvailable }: Props) =
     if(numberOfRewardsAvailable === 0) {
         return <><Box as={'span'} color={'neutral.600'} fontWeight={700}>{t('Sold Out')}</Box> <Box as={'span'} style={{fontSize: "10px", position: "relative", top: "-2px"}}>&#8226;</Box> </>;
     } 
-    else if ( numberOfRewardsAvailable > 3 ) {
-        return <><Box as={'span'}>{numberOfRewardsAvailable + ` ${t('remaining')}`}</Box> <Box as={'span'} style={{fontSize: "10px", position: "relative", top: "-2px"}}>&#8226;</Box> </>;
-    } 
-    else if ( numberOfRewardsAvailable > 0 && numberOfRewardsAvailable <= 3 ) {
-        return <><Box as={'span'} color={'secondary.red'}>{numberOfRewardsAvailable + ` ${t('remaining')}`}</Box> <Box as={'span'} style={{fontSize: "10px", position: "relative", top: "-2px"}}>&#8226;</Box> </>;
+    else if ( numberOfRewardsAvailable > 0 ) {
+        return <><Box as={'span'} color={'#DD6B20'}>{numberOfRewardsAvailable + ` ${t('remaining')}`}</Box> <Box as={'span'} style={{fontSize: "10px", position: "relative", top: "-2px"}}>&#8226;</Box> </>;
     }
     return <></>;
 }
