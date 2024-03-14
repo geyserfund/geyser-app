77962b53f3a1be19642c291d2ed5a05ed4c38e9a
sajald77 <sazzaldulal@gmail.com>
2024-03-14 16:04:35 -0400

    changeset: minor update to label

--- src/pages/projectDashboard/ProjectDashboard.tsx
+++ src/pages/projectDashboard/ProjectDashboard.tsx
@@ -39,7 +39,7 @@ export const projectSections: Record<string, DashboardSection> = {
     semiFullWidth: true,
   },
   rewards: {
-    label: 'Denominations',
+    label: 'Currency Denominations',
     path: 'dashboardRewards',
   },
   wallet: {
