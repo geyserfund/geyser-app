edbb331ccccee9ca34acee67b27d2124daddaa0c
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-03-27 17:19:03 -0300

    fix: nicer not found page

--- src/pages/fallback/CommonFeedbackMessage.tsx
+++ src/pages/fallback/CommonFeedbackMessage.tsx
@@ -5,15 +5,33 @@ import { GeyserFeedbackFromUrl } from '../../constants'
 
 export const CommonFeedbackMessage = () => {
   return (
-    <Text fontSize="20px">
-      <Trans i18nKey={'If the problem persists let us know via. <1>telegram</1> or this <3>feedback form.</3>'}>
-        {'If the problem persists let us know via. '}
-        <Link key="telegram" href="https://t.me/+EZ5otIPhVcxhMmFk" isExternal>
-          telegram
+    <Text variant="body1">
+      <Trans
+        i18nKey={'If the issue persists let us know through our <1>feedback form</3> or reach out on <3>Telegram</1>'}
+      >
+        {'If the issue persists let us know through our '}
+        <Link
+          key="feedback"
+          href={GeyserFeedbackFromUrl}
+          isExternal
+          textDecoration="none"
+          variant="body1"
+          fontWeight="700"
+        >
+          feedback form
         </Link>
-        {' or this '}
-        <Link key="feedback" href={GeyserFeedbackFromUrl} isExternal>
-          feedback form.
+        {' or reach out on '}
+        <Link
+          key="telegram"
+          href="https://t.me/+EZ5otIPhVcxhMmFk"
+          isExternal
+          textDecoration="none"
+          variant="body1"
+          fontWeight="700"
+        >
+          <Text variant="body1" fontWeight="700">
+            Telegram.
+          </Text>
         </Link>
       </Trans>
     </Text>
