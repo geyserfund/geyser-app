f81ed230c80177511d2481489efca859f9105313
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-03-28 11:12:05 -0300

    fix: similar styling to not authorized

--- src/pages/fallback/NotAuthorized.tsx
+++ src/pages/fallback/NotAuthorized.tsx
@@ -3,25 +3,29 @@ import { useTranslation } from 'react-i18next'
 import { BiErrorAlt } from 'react-icons/bi'
 
 import { CommonFeedbackMessage } from './CommonFeedbackMessage'
+import { Head } from '../../config'
 
 export const NotAuthorized = () => {
   const { t } = useTranslation()
   return (
-    <VStack
-      width="100%"
-      height="100%"
-      backgroundColor="neutral.100"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      spacing="10px"
-    >
-      <BiErrorAlt fontSize="80px" />
-      <Text fontSize="20px">{t('Oops!')}</Text>
-      <Text fontSize="20px">{t('You do not have permission to access this page.')}</Text>
-      <CommonFeedbackMessage prefix={''}/>
-    </VStack>
+    <>
+      <Head title="Not authorized" />
+      <VStack
+        width="100%"
+        height="100%"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing="10px"
+      >
+        <BiErrorAlt fontSize="80px" />
+        <Text variant="h2" textAlign="center">
+          {t('Oops!')} {t('You do not have permission to access this page.')}
+        </Text>
+        <CommonFeedbackMessage prefix={''}/>
+      </VStack>
+    </>
   )
 }
 
