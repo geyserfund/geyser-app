a45917ffd6385aad7d8f34d2be94252efaa6ac90
sajald77 <sazzaldulal@gmail.com>
2024-04-17 13:22:31 -0400

    fix: remove fee reduction for display

--- src/utils/helpers/projectHelpers.ts
+++ src/utils/helpers/projectHelpers.ts
@@ -1,15 +1,7 @@
-import { GEYSER_FEE, noFeeProjects, WalletConnectDetails } from '../../constants'
+import { WalletConnectDetails } from '../../constants'
 import { ProjectFragment } from '../../types'
 
 type PickProject = Pick<ProjectFragment, 'name' | 'balance' | 'wallets'>
-
-export const getProjectBalance = (project: PickProject) => {
-  if (noFeeProjects.includes(project.name) || hasOwnNode(project)) {
-    return project.balance
-  }
-
-  return Math.round(project.balance * (1 - GEYSER_FEE))
-}
 
 export const hasOwnNode = (project: PickProject) => {
   const currentWallet = project.wallets && project.wallets[0]
