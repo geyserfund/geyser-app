25ece0c4f2f6cd80ab5cc51e5c128052a640a71e
sajald77 <sazzaldulal@gmail.com>
2024-04-25 11:40:09 -0400

    fix: remove unnecessary button

--- src/modules/project/pages/projectView/views/projectActivityPanel/screens/qr/views/onchain/views/OnChainQR.tsx
+++ src/modules/project/pages/projectView/views/projectActivityPanel/screens/qr/views/onchain/views/OnChainQR.tsx
@@ -7,13 +7,10 @@ import { QRCodeComponent } from '../../../components/QRCodeComponent'
 import { WaitingForPayment } from '../../../components/WaitingForPayment'
 import { useCopyToClipboard } from '../../../hooks/useCopyButton'
 import { PaymentAndRefundInstructions } from '../components'
-import { useGoToNextOnChainStatus } from '../states/onChainStatus'
 
 export const OnChainQR = ({ onChainAddress }: { onChainAddress: string }) => {
   const { t } = useTranslation()
   const { onCopy, hasCopied } = useCopyToClipboard(onChainAddress)
-
-  const goToNextOnChainStatus = useGoToNextOnChainStatus()
 
   return (
     <VStack flexWrap="wrap" maxWidth="100%" spacing="20px">
@@ -32,11 +29,6 @@ export const OnChainQR = ({ onChainAddress }: { onChainAddress: string }) => {
       >
         <Text>{hasCopied ? t('Copied!') : t('Copy onchain address')}</Text>
       </Button>
-
-      <Button size="md" onClick={goToNextOnChainStatus} variant="secondary" width={'100%'}>
-        goToNextOnChainStatus
-      </Button>
-
       <PaymentAndRefundInstructions />
     </VStack>
   )
