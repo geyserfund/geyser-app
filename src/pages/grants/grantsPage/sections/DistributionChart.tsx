c841247ec2a8fec0caf855b55bbe48f94de41848
sajald77 <sazzaldulal@gmail.com>
2024-05-03 18:06:10 -0400

    chore: cleanup

--- src/pages/grants/grantsPage/sections/DistributionChart.tsx
+++ src/pages/grants/grantsPage/sections/DistributionChart.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, HStack, Text, VStack } from '@chakra-ui/react'
+import { Box, BoxProps, HStack, Text } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 
 import { CardLayout } from '../../../../components/layouts'
