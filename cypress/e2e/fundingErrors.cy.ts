dc04411ee8a27196d90850d14057f887f1e1592a
sajald77 <sazzaldulal@gmail.com>
2024-05-21 08:57:50 -0400

    chore: uncomment all tests

--- cypress/e2e/fundingErrors.cy.ts
+++ cypress/e2e/fundingErrors.cy.ts
@@ -22,56 +22,56 @@ describe('When fund mutation fails', () => {
     })
   })
 
-  // context('when invoice is lower than limit', () => {
-  //   it('should show wallet min error page', () => {
-  //     errorFlowForFundingMutation({
-  //       code: ApolloErrors.INVALID_FUNDING_AMOUNT,
-  //       minAmount: 80000,
-  //     })
+  context('when invoice is lower than limit', () => {
+    it('should show wallet min error page', () => {
+      errorFlowForFundingMutation({
+        code: ApolloErrors.INVALID_FUNDING_AMOUNT,
+        minAmount: 80000,
+      })
 
-  //     cy.get('h2').contains('Receiver’s Wallet Transaction Below Minimum Limit').should('be.visible')
-  //   })
-  // })
+      cy.get('h2').contains('Receiver’s Wallet Transaction Below Minimum Limit').should('be.visible')
+    })
+  })
 
-  // context('when wallet is unreachable', () => {
-  //   it('should show wallet unreachable error page', () => {
-  //     errorFlowForFundingMutation({
-  //       code: ApolloErrors.WALLET_UNREACHABLE,
-  //     })
+  context('when wallet is unreachable', () => {
+    it('should show wallet unreachable error page', () => {
+      errorFlowForFundingMutation({
+        code: ApolloErrors.WALLET_UNREACHABLE,
+      })
 
-  //     cy.get('h2').contains('Wallet Unreachable').should('be.visible')
-  //   })
-  // })
+      cy.get('h2').contains('Wallet Unreachable').should('be.visible')
+    })
+  })
 
-  // context('when project is inactive', () => {
-  //   it('should show project inactive error page', () => {
-  //     errorFlowForFundingMutation({
-  //       code: ApolloErrors.NON_ACTIVE_PROJECT,
-  //     })
+  context('when project is inactive', () => {
+    it('should show project inactive error page', () => {
+      errorFlowForFundingMutation({
+        code: ApolloErrors.NON_ACTIVE_PROJECT,
+      })
 
-  //     cy.get('h2').contains('Inactive Project').should('be.visible')
-  //   })
-  // })
+      cy.get('h2').contains('Inactive Project').should('be.visible')
+    })
+  })
 
-  // context('when reward is unavilable', () => {
-  //   it('should show reward outofstock error page', () => {
-  //     errorFlowForFundingMutation({
-  //       code: ApolloErrors.REWARD_OUT_OF_STOCK,
-  //     })
+  context('when reward is unavilable', () => {
+    it('should show reward outofstock error page', () => {
+      errorFlowForFundingMutation({
+        code: ApolloErrors.REWARD_OUT_OF_STOCK,
+      })
 
-  //     cy.get('h2').contains('Rewards Out of Stock').should('be.visible')
-  //   })
-  // })
+      cy.get('h2').contains('Rewards Out of Stock').should('be.visible')
+    })
+  })
 
-  // context('when server responds with internal server error', () => {
-  //   it('should show general error page', () => {
-  //     errorFlowForFundingMutation({
-  //       code: ApolloErrors.INTERNAL_SERVER_ERROR,
-  //     })
+  context('when server responds with internal server error', () => {
+    it('should show general error page', () => {
+      errorFlowForFundingMutation({
+        code: ApolloErrors.INTERNAL_SERVER_ERROR,
+      })
 
-  //     cy.get('h2').contains('An Error Occured').should('be.visible')
-  //   })
-  // })
+      cy.get('h2').contains('An Error Occured').should('be.visible')
+    })
+  })
 })
 
 const errorFlowForFundingMutation = (errorExtension: ErrorExtensionType) => {
