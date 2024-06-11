3e6595aacb06399443a546787d9a322bb42acf55
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-06-11 11:38:00 -0400

    feat: need to login to vote

--- src/pages/grants/grantsPage/sections/CommunityVoting.tsx
+++ src/pages/grants/grantsPage/sections/CommunityVoting.tsx
@@ -1,7 +1,10 @@
+import { useDisclosure } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 
 import { CardLayout } from '../../../../components/layouts'
+import { AuthModal } from '../../../../components/molecules'
 import { H3 } from '../../../../components/typography'
+import { useAuthContext } from '../../../../context'
 import { ProjectFundingModal } from '../../../../modules/project/pages/projectFunding/components/ProjectFundingModal'
 import { GrantApplicant, GrantStatusEnum } from '../../../../types'
 import { GrantApplicantCard } from '../components/GrantApplicantCard'
@@ -29,7 +32,9 @@ export const CommunityVoting = ({
   isCompetitionVote,
 }: Props) => {
   const { t } = useTranslation()
-  const modalProps = useProjectFundingModal()
+  const fundingModalProps = useProjectFundingModal()
+  const { onOpen, onClose, isOpen } = useDisclosure()
+  const { isLoggedIn } = useAuthContext()
 
   if (!applicants) {
     return null
@@ -38,26 +43,37 @@ export const CommunityVoting = ({
   const canVote = grantHasVoting && grantStatus === GrantStatusEnum.FundingOpen
 
   return (
-    <CardLayout noMobileBorder p={{ base: '10px', lg: '20px' }} spacing={{ base: '10px', lg: '20px' }} w="full">
-      <H3 fontSize="18px">{t(title)}</H3>
-      {applicants.map(({ project, funding, contributors, contributorsCount }) => {
-        return (
-          <GrantApplicantCard
-            key={project.name}
-            project={project}
-            funding={funding}
-            contributorsCount={contributorsCount}
-            contributors={contributors || []}
-            grantHasVoting={grantHasVoting || false}
-            grantStatus={grantStatus as GrantStatusEnum}
-            isClosed={isClosed || false}
-            isCompetitionVote={isCompetitionVote || false}
-            modalProps={modalProps}
-            canVote={canVote || false}
-          />
-        )
-      })}
-      {modalProps.isOpen && <ProjectFundingModal {...modalProps} />}
-    </CardLayout>
+    <>
+      <CardLayout noMobileBorder p={{ base: '10px', lg: '20px' }} spacing={{ base: '10px', lg: '20px' }} w="full">
+        <H3 fontSize="18px">{t(title)}</H3>
+        {applicants.map(({ project, funding, contributors, contributorsCount }) => {
+          return (
+            <GrantApplicantCard
+              key={project.name}
+              project={project}
+              funding={funding}
+              contributorsCount={contributorsCount}
+              contributors={contributors || []}
+              grantHasVoting={grantHasVoting || false}
+              grantStatus={grantStatus as GrantStatusEnum}
+              isLoggedIn={isLoggedIn}
+              isClosed={isClosed || false}
+              isCompetitionVote={isCompetitionVote || false}
+              fundingModalProps={fundingModalProps}
+              canVote={canVote || false}
+              onOpenLoginModal={onOpen}
+            />
+          )
+        })}
+        {fundingModalProps.isOpen && <ProjectFundingModal {...fundingModalProps} />}
+
+        <AuthModal
+          title={t('Login to vote')}
+          description={t('You need to login to vote for this community voting grant. ')}
+          isOpen={isOpen}
+          onClose={onClose}
+        />
+      </CardLayout>
+    </>
   )
 }
