64f646efdf61226d52c726361ee3c7bbf66e1f47
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-06-14 10:58:46 -0400

    feat: adapt project contributors modal to be used in grants

--- src/modules/project/pages/projectView/views/projectActivityPanel/screens/info/components/ProjectContributorsList.tsx
+++ src/modules/project/pages/projectView/views/projectActivityPanel/screens/info/components/ProjectContributorsList.tsx
@@ -7,13 +7,13 @@ import { ID } from '../../../../../../../../../constants'
 import { QUERY_PROJECT_FUNDERS } from '../../../../../../../../../graphql'
 import { ScrollInvoke } from '../../../../../../../../../helpers'
 import { useQueryWithPagination } from '../../../../../../../../../hooks'
-import { FunderWithUserFragment, ProjectFragment } from '../../../../../../../../../types'
+import { FunderWithUserFragment, Project, ProjectFragment } from '../../../../../../../../../types'
 import { useMobileMode, useNotification } from '../../../../../../../../../utils'
 
 interface ProjectContributorsListProps extends CardLayoutProps {
   id?: string
   isBounded?: boolean
-  project: ProjectFragment
+  project: ProjectFragment | Project
 }
 
 const CONTRIBUTORS_ITEM_LIMIT = 50
