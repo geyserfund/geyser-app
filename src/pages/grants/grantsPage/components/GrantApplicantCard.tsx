1cebff2e88fa14a85e839dd646a91b22ee90d45b
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-06-14 10:58:46 -0400

    feat: adapt project contributors modal to be used in grants

--- src/pages/grants/grantsPage/components/GrantApplicantCard.tsx
+++ src/pages/grants/grantsPage/components/GrantApplicantCard.tsx
@@ -20,6 +20,7 @@ import {
   GrantApplicantFunding,
   GrantStatusEnum,
   Project,
+  ProjectFragment,
   UserMeFragment,
 } from '../../../../types'
 import { getShortAmountLabel, useMobileMode } from '../../../../utils'
