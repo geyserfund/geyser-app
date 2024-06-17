f5821ffb07c24bf50f13580c1fdcfd6f432991bd
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-06-17 12:43:30 -0400

    fix: remove unused import

--- src/pages/otp/VerifyYourEmailContent.tsx
+++ src/pages/otp/VerifyYourEmailContent.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Image } from '@chakra-ui/react'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { BsArrowLeft } from 'react-icons/bs'
