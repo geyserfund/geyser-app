e5390a239d02e443f96f0acca3b02766dde462b0
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-06-20 11:55:11 -0400

    fix: simple contribution success modal

--- src/modules/project/pages/projectFunding/views/FundingComplete.tsx
+++ src/modules/project/pages/projectFunding/views/FundingComplete.tsx
@@ -1,17 +1,22 @@
-import { Box, Link as ChakraLink, Text, VStack } from '@chakra-ui/react'
-import { Trans, useTranslation } from 'react-i18next'
+import { Box, Button, Text, VStack } from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
 import { FaCheck } from 'react-icons/fa'
 
-import { useFundingContext } from '../../../context/FundingProvider'
+import { ProjectFragment } from '../../../../../types'
 import { ProjectFundingFormState } from './FundingForm'
 
 interface Props {
   formState: ProjectFundingFormState | undefined
+  project: ProjectFragment
 }
 
-export const FundingComplete = ({ formState }: Props) => {
+export const FundingComplete = ({ formState, project }: Props) => {
   const { t } = useTranslation()
-  const { fundingTx } = useFundingContext()
+
+  const handleClose = () => {
+    window.location.reload()
+  }
+
   return (
     <VStack justify={'center'} spacing={5}>
       <Box display="flex" justifyContent={'center'} my={4}>
@@ -27,32 +32,22 @@ export const FundingComplete = ({ formState }: Props) => {
           <FaCheck />
         </Box>
       </Box>
-      {formState && (
-        <Text fontSize={'14px'}>
-          <Trans
-            i18nKey="Your <1>{{amount}}</1>> contribution to Geyser Grants Round 2 was successful!"
-            values={{ amount: `${formState.donationAmount} sats` }}
-          >
-            Your
-            <span style={{ fontWeight: 'bold' }}>{'{{amount}}'}</span> contribution to Geyser Grants Round 2 was
-            successful!
-          </Trans>
-        </Text>
-      )}
-      <Text fontSize={'14px'}>
-        {t('Your donation will help accelerate bitcoin adoption by recognizing and pushing forward bitcoin projects.')}
-      </Text>
-      <Text fontSize={'14px'}>{t('Donations are non-refundable and not tax deductible.')}</Text>
-      {fundingTx.onChain && (
-        <Text mt={4} fontSize={'14px'}>
-          {t('Check out')}{' '}
-          <ChakraLink href={`https://mempool.space/address/${fundingTx.address}`}>
-            <Box as="span" fontWeight="bold" borderBottom="1px solid" borderColor="neutral.1000">
-              {t('the block explorer')}
-            </Box>
-          </ChakraLink>
-        </Text>
-      )}
+      <VStack>
+        {formState && (
+          <Text fontSize={'14px'}>
+            {t('You sent')} <Text as="span" fontWeight="bold">{`${formState.donationAmount} sats`}</Text> {t('to')}{' '}
+            <Text as="span" fontWeight="bold">
+              {project.name}
+            </Text>
+            !
+          </Text>
+        )}
+        <Text fontSize={'14px'}>{t('Thank you for contributing to Bitcoin projects.')}</Text>
+
+        <Button variant={'primary'} onClick={handleClose}>
+          {t('Close')}
+        </Button>
+      </VStack>
     </VStack>
   )
 }
