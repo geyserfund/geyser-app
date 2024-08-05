0a9d60a9022151bde14d84e72cd8ab381fe8704c
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-08-05 07:20:26 -0400

    feat: query notifications data

---
+++ src/modules/profile/pages/profileSettings/components/CreatorNotifications.tsx
@@ -0,0 +1,185 @@
+import { useQuery } from '@apollo/client'
+import { Avatar, FormControl, FormLabel, HStack, Select, Switch, VStack } from '@chakra-ui/react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Controller, useForm } from 'react-hook-form'
+import { useParams } from 'react-router'
+import * as yup from 'yup'
+
+import { QUERY_CREATOR_NOTIFICATIONS_SETTINGS } from '@/modules/profile/graphql/queries/creatorNotificationsSettingsQuery'
+import { ControlledSelect } from '@/shared/components/controlledInput'
+import { Body } from '@/shared/components/typography'
+
+// Define the form schema using Yup
+const schema = yup.object().shape({
+  projects: yup.array().of(
+    yup.object().shape({
+      id: yup.string().required(),
+      creatorSummary: yup.boolean(),
+      creatorSummaryFrequency: yup.string().when('creatorSummary', {
+        is: true,
+        then: (schema) => schema.required('Please select a frequency'),
+      }),
+      goalReached: yup.boolean(),
+      saleMade: yup.boolean(),
+      contributionReceived: yup.boolean(),
+      contributionThreshold: yup.string().when('contributionReceived', {
+        is: true,
+        then: (schema) => schema.required('Please select a threshold'),
+      }),
+    }),
+  ),
+})
+
+export const CreatorNotifications = () => {
+  const { userId } = useParams()
+
+  const { data } = useQuery(QUERY_CREATOR_NOTIFICATIONS_SETTINGS, {
+    variables: { userId },
+    onCompleted(data) {
+      console.log(data)
+    },
+  })
+
+  const settings = data?.settingsNotificationsCreatorGet
+  const project = settings?.project
+
+  const { control, handleSubmit, getValues } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      project: {
+        id: project?.id,
+        creatorSummary:
+          settings?.notificationSettings.find((s: any) => s.notificationType === 'creator.projectSummary')?.isEnabled ||
+          false,
+        creatorSummaryFrequency:
+          settings?.notificationSettings
+            .find((s: any) => s.notificationType === 'creator.projectSummary')
+            ?.configurations.find((c: any) => c.name === 'frequency')?.value || '',
+        goalReached:
+          settings?.notificationSettings.find((s: any) => s.notificationType === 'creator.goalReached')?.isEnabled ||
+          false,
+        saleMade:
+          settings?.notificationSettings.find((s: any) => s.notificationType === 'creator.saleMade')?.isEnabled ||
+          false,
+        contributionReceived:
+          settings?.notificationSettings.find((s: any) => s.notificationType === 'creator.contributionReceived')
+            ?.isEnabled || false,
+        contributionThreshold:
+          settings?.notificationSettings
+            .find((s: any) => s.notificationType === 'creator.contributionReceived')
+            ?.configurations.find((c: any) => c.name === 'threshold')?.value || '',
+      },
+    },
+  })
+
+  console.log(getValues())
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+    console.log(getValues())
+    // Handle form submission
+  }
+
+  return (
+    <form style={{ width: '100%' }} onSubmit={handleSubmit(onSubmit)}>
+      <VStack spacing={6} align="stretch">
+        <Body size="lg" bold>
+          Creator notifications
+        </Body>
+        {project && (
+          <VStack key={project.id} spacing={4} align="stretch" p={4}>
+            <HStack>
+              <Avatar src={project.avatarUrl} name={project.title} size="sm" />
+              <Body bold>{project.title}</Body>
+            </HStack>
+
+            <FormControl display="flex" alignItems="center" justifyContent="space-between">
+              <FormLabel htmlFor={`creator-summary-${project.id}`} mb="0">
+                Creator summary email
+              </FormLabel>
+              <HStack>
+                <ControlledSelect
+                  name={`project.creatorSummaryFrequency`}
+                  control={control}
+                  label=""
+                  options={[
+                    { value: 'weekly', label: 'Weekly' },
+                    { value: 'monthly', label: 'Monthly' },
+                  ]}
+                  placeholder="Select frequency"
+                  width="auto"
+                />
+                <Controller
+                  name={`project.creatorSummary`}
+                  control={control}
+                  render={({ field: { onChange, value, ref } }) => (
+                    <Switch id={`creator-summary-${project.id}`} isChecked={value} onChange={onChange} ref={ref} />
+                  )}
+                />
+              </HStack>
+            </FormControl>
+
+            <FormControl display="flex" alignItems="center" justifyContent="space-between">
+              <FormLabel htmlFor={`goal-reached-${project.id}`} mb="0">
+                Goal Reached Email
+              </FormLabel>
+              <Controller
+                name={`project.goalReached`}
+                control={control}
+                render={({ field: { onChange, value, ref } }) => (
+                  <Switch id={`goal-reached-${project.id}`} isChecked={value} onChange={onChange} ref={ref} />
+                )}
+              />
+            </FormControl>
+
+            <FormControl display="flex" alignItems="center" justifyContent="space-between">
+              <FormLabel htmlFor={`sale-made-${project.id}`} mb="0">
+                Sale Made
+              </FormLabel>
+              <Controller
+                name={`project.saleMade`}
+                control={control}
+                render={({ field: { onChange, value, ref } }) => (
+                  <Switch id={`sale-made-${project.id}`} isChecked={value} onChange={onChange} ref={ref} />
+                )}
+              />
+            </FormControl>
+
+            <FormControl display="flex" alignItems="center" justifyContent="space-between">
+              <FormLabel htmlFor={`contribution-received-${project.id}`} mb="0">
+                Contribution received
+              </FormLabel>
+              <HStack>
+                <ControlledSelect
+                  name={`project.contributionThreshold`}
+                  control={control}
+                  label=""
+                  options={[
+                    { value: '10000', label: '> 10,000' },
+                    { value: '50000', label: '> 50,000' },
+                    { value: '100000', label: '> 100,000' },
+                    { value: 'all', label: 'All' },
+                  ]}
+                  placeholder="Select threshold"
+                  width="auto"
+                />
+                <Controller
+                  name={`project.contributionReceived`}
+                  control={control}
+                  render={({ field: { onChange, value, ref } }) => (
+                    <Switch
+                      id={`contribution-received-${project.id}`}
+                      isChecked={value}
+                      onChange={onChange}
+                      ref={ref}
+                    />
+                  )}
+                />
+              </HStack>
+            </FormControl>
+          </VStack>
+        )}
+      </VStack>
+    </form>
+  )
+}
--- src/modules/profile/pages/profileSettings/views/ProfileSettingsNotifications.tsx
+++ src/modules/profile/pages/profileSettings/views/ProfileSettingsNotifications.tsx
@@ -1,12 +1,27 @@
-import { VStack } from '@chakra-ui/react'
+import { Button, Divider, HStack, VStack } from '@chakra-ui/react'
+
+import { Body } from '@/shared/components/typography'
 
 import { ProfileSettingsLayout } from '../common/ProfileSettingsLayout'
+import { CreatorNotifications } from '../components/CreatorNotifications'
 
 export const ProfileSettingsNotifications = () => {
   return (
     <ProfileSettingsLayout>
       <VStack w="100%" spacing={6} flexGrow={1} px={{ base: 0, lg: 6 }}>
-        <p>ProfileSettingsNotifications</p>
+        <VStack w="100%" alignItems="flex-start">
+          <Body size="lg" bold>
+            Notifications
+          </Body>
+          <HStack w="100%" justifyContent="space-between">
+            <Body>Customize your profile and project notifications.</Body>
+            <Button variant="outline" colorScheme="neutral1">
+              <Body>Set to default</Body>
+            </Button>
+          </HStack>
+        </VStack>
+        <CreatorNotifications />
+        <Divider />
       </VStack>
     </ProfileSettingsLayout>
   )
