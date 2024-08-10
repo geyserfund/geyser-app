a60bdab4d9121f5f64bf7e7ef4e964ffa675f869
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-08-10 14:40:51 -0400

    feat: banner styling

--- src/modules/leaderboard/components/SummaryBanner.tsx
+++ src/modules/leaderboard/components/SummaryBanner.tsx
@@ -1,8 +1,9 @@
-import { HStack, Skeleton, Text, VStack } from '@chakra-ui/react'
+import { Box, HStack, Skeleton, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { useMobileMode } from '@/utils'
+import { Body } from '@/shared/components/typography'
+import { convertToBTC, getBitcoinAmount, getShortAmountLabel, useMobileMode } from '@/utils'
 
 import { useSummaryBannerStats } from '../hooks'
 
@@ -11,26 +12,55 @@ export const SummaryBanner = () => {
   const isMobile = useMobileMode()
   const { projectsCount, bitcoinsRaised, contributorsCount, loading } = useSummaryBannerStats()
 
-  const Direction = isMobile ? VStack : HStack
+  const Direction = isMobile ? HStack : VStack
+  const Column = isMobile ? VStack : HStack
 
   return (
-    <Direction width="full" justifyContent="space-around" bg="gray.100" p={4} borderRadius="md" spacing={4}>
-      <BannerItem label={t('Projects')} value={loading ? <Skeleton height="20px" width="60px" /> : projectsCount} />
-      <BannerItem
-        label={t('Bitcoins raised')}
-        value={loading ? <Skeleton height="20px" width="60px" /> : bitcoinsRaised}
-      />
-      <BannerItem
-        label={t('Contributors')}
-        value={loading ? <Skeleton height="20px" width="60px" /> : contributorsCount}
-      />
+    <Direction
+      width="full"
+      border="1px solid"
+      justifyContent="center"
+      borderColor="primaryAlpha.6"
+      bgGradient="linear(to-r, #B2FAEC, #F1FAFD)"
+      p={4}
+      borderRadius="md"
+      height="130px"
+      maxHeight="130px"
+    >
+      <Box>
+        <Body fontSize={isMobile ? '16px' : '20px'} color="primary1.12">
+          {t('The leaders making world-changing ideas possible')}
+        </Body>
+      </Box>
+      <Column w={isMobile ? '100%' : 'auto'} spacing={isMobile ? 2 : 6} alignItems={isMobile ? 'flex-end' : 'center'}>
+        <BannerItem
+          label={t('Projects')}
+          value={loading ? <Skeleton height="20px" width="60px" /> : getShortAmountLabel(projectsCount)}
+        />
+        <BannerItem
+          label={t('Bitcoin raised')}
+          value={loading ? <Skeleton height="20px" width="60px" /> : getBitcoinAmount(bitcoinsRaised, true)}
+        />
+        <BannerItem
+          label={t('Contributors')}
+          value={loading ? <Skeleton height="20px" width="60px" /> : getShortAmountLabel(contributorsCount)}
+        />
+      </Column>
     </Direction>
   )
 }
 
-const BannerItem = ({ label, value }: { label: string; value: React.ReactNode }) => (
-  <VStack>
-    <Text fontWeight="bold">{label}</Text>
-    <Text>{value}</Text>
-  </VStack>
-)
+const BannerItem = ({ label, value }: { label: string; value: React.ReactNode }) => {
+  const isMobile = useMobileMode()
+
+  return (
+    <HStack>
+      <Body fontSize={isMobile ? '16px' : '24px'} color="neutral1.12" bold>
+        {value}
+      </Body>
+      <Body fontSize={isMobile ? '16px' : '24px'} color="neutral1.12" bold>
+        {label}
+      </Body>
+    </HStack>
+  )
+}
