7f68805619ace22da4a11d805e4c2a1f71ea843e
sajald77 <sazzaldulal@gmail.com>
2024-08-12 15:54:48 -0400

    fix: funding error tests

--- cypress/e2e/fundingErrors.cy.ts
+++ cypress/e2e/fundingErrors.cy.ts
@@ -8,6 +8,8 @@ const FUNDING_COMMENT = 'This was the test comment'
 
 describe('When fund mutation fails', () => {
   beforeEach(() => {
+    console.log('chekcing geyser url', GEYSER_URL)
+    cy.log('chekcing geyser url', GEYSER_URL)
     cy.visit(`${GEYSER_URL}/project/${LND_TEST_PROJECT_NAME}`)
   })
 
@@ -17,8 +19,7 @@ describe('When fund mutation fails', () => {
         code: ApolloErrors.INVALID_FUNDING_AMOUNT,
         maxAmount: 50000,
       })
-
-      cy.get('h2').contains('Receiver’s Wallet maximum limit Reached').should('be.visible')
+      cy.get('h2').contains('Receiver’s Wallet Transaction Above Maximum Limit').should('be.visible')
     })
   })
 
