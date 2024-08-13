4cd50855f70df1c6d310eccf842f35e0d46d4233
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-08-13 17:11:40 -0400

    feat: header grants

---
+++ src/modules/grants/components/GrantsHeader.tsx
@@ -0,0 +1,56 @@
+import { useQuery } from '@apollo/client'
+import { Button, HStack, VStack } from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
+
+import { DonateIcon } from '@/components/icons/svg/DonateIcon'
+import { TrophyIcon } from '@/components/icons/svg/TrophyIcon'
+import { Banner } from '@/components/ui/Banner'
+import { QUERY_GRANT_STATISTICS } from '@/graphqlBase/queries/grant'
+import { GrantStatistics } from '@/types'
+import { getShortAmountLabel, useMobileMode } from '@/utils'
+
+const GrantsHeader = () => {
+  const { t } = useTranslation()
+
+  const isMobile = useMobileMode()
+
+  const { data, loading } = useQuery<{ grantStatistics: GrantStatistics }>(QUERY_GRANT_STATISTICS)
+
+  const items = [
+    {
+      label: t('Granted'),
+      value: getShortAmountLabel(data?.grantStatistics.grants?.amountFunded || 0),
+      suffix: 'Sats',
+    },
+    {
+      label: t('Distributed'),
+      value: getShortAmountLabel(data?.grantStatistics.grants?.amountGranted || 0),
+      suffix: 'Sats',
+    },
+  ]
+
+  const Direction = isMobile ? VStack : HStack
+
+  return (
+    <VStack spacing={4}>
+      <Banner title={t('Geyser Grants - empowering bitcoin creators!')} items={items} loading={loading} reverse />
+      <Direction
+        justifyContent="space-between"
+        w="100%"
+        border="1px solid"
+        borderColor="neutralAlpha.6"
+        borderRadius="md"
+        p={6}
+      >
+        <Button w="100%" size="lg" variant="primary" rightIcon={<DonateIcon />}>
+          {t('Donate to Geyser Grant')}
+        </Button>
+        <Button w="100%" size="lg" variant="primary" bg="yellow.9" rightIcon={<TrophyIcon />}>
+          {t('Create a grant')}
+        </Button>
+      </Direction>
+    </VStack>
+  )
+}
+
+export default GrantsHeader
