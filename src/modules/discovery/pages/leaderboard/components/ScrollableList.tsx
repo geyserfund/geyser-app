f9cb52bb68348ca337aaf0e573ad17350c805eca
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-08-26 16:55:32 -0400

    fix: remove sx styling

--- src/modules/discovery/pages/leaderboard/components/ScrollableList.tsx
+++ src/modules/discovery/pages/leaderboard/components/ScrollableList.tsx
@@ -1,15 +1,12 @@
 import { StackProps, VStack } from '@chakra-ui/react'
 import React from 'react'
 
-import { useMobileMode } from '@/utils'
-
 interface ScrollableListProps<T> extends StackProps {
   data: T[]
   renderItem: (item: T, index: number) => React.ReactNode
 }
 
 export function ScrollableList<T>({ data, renderItem, ...props }: ScrollableListProps<T>) {
-  const isMobile = useMobileMode()
   return (
     <VStack
       width="full"
@@ -17,23 +14,6 @@ export function ScrollableList<T>({ data, renderItem, ...props }: ScrollableList
       maxHeight={{ base: '100%', lg: '715px' }}
       overflowY="auto"
       overflowX="hidden"
-      sx={{
-        '&::-webkit-scrollbar': {
-          width: '4px',
-        },
-        '&::-webkit-scrollbar-track': {
-          background: 'neutralAlpha.3',
-          borderRadius: 'full',
-        },
-        '&::-webkit-scrollbar-thumb': {
-          background: 'neutralAlpha.8',
-          borderRadius: 'full',
-          '&:hover': {
-            background: 'neutralAlpha.9',
-          },
-        },
-        scrollbarWidth: isMobile ? 'none' : '4px',
-      }}
       {...props}
     >
       {data.map((item, index) => renderItem(item, index))}
