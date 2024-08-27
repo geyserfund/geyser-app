1b92dde20fe71b553525b540592dce2fa8ccc0c1
sajald77 <sazzaldulal@gmail.com>
2024-08-27 10:02:07 -0400

    fix: layout issue with discovery and activity

--- src/modules/discovery/pages/activity/components/ProjectsIFollowFeed.tsx
+++ src/modules/discovery/pages/activity/components/ProjectsIFollowFeed.tsx
@@ -1,6 +1,7 @@
 import { Skeleton, VStack } from '@chakra-ui/react'
 
 import { ScrollInvoke } from '@/helpers'
+import { ID } from '@/shared/constants'
 import { Activity, ActivityFeedName } from '@/types'
 import { useMobileMode } from '@/utils'
 
@@ -14,19 +15,17 @@ const ProjectsIFollow = () => {
     ActivityFeedName.FollowedProjects,
   )
 
-  const id = 'activity-scroll-container'
-
   if (isLoading) {
     return <ProjectsIFollowSkeleton />
   }
 
   return (
-    <VStack h="full" id={id} py={2} overflowY={{ base: undefined, lg: 'auto' }} width="full" spacing={4}>
+    <VStack h="full" py={2} width="full" spacing={4}>
       {followedProjectsActivities.map((item) => (
         <ActivityItem key={item.id} {...(item as Activity)} />
       ))}
       <ScrollInvoke
-        elementId={!isMobile ? id : undefined}
+        elementId={!isMobile ? ID.root : undefined}
         onScrollEnd={fetchNext}
         isLoading={isLoadingMore}
         noMoreItems={noMoreItems}
@@ -37,7 +36,7 @@ const ProjectsIFollow = () => {
 
 const ProjectsIFollowSkeleton = () => {
   return (
-    <VStack h="full" py={2} overflowY={{ base: undefined, lg: 'auto' }} width="full" spacing={4}>
+    <VStack h="full" py={2} width="full" spacing={4}>
       {[1, 2, 3, 4, 5, 6].map((item) => {
         return <ActivityItemSkeleton key={item} />
       })}
