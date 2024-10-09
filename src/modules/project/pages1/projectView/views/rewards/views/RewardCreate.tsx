c3b8f8f8923c5e93c94fb32f14cec024900552d0
Malik Aghiles Tahir <ag.malik.tahir@gmail.com>
2024-10-09 19:11:03 +0100

    fix: remove old reward form

--- src/modules/project/pages1/projectView/views/rewards/views/RewardCreate.tsx
+++ src/modules/project/pages1/projectView/views/rewards/views/RewardCreate.tsx
@@ -1,42 +1,14 @@
 import { VStack } from '@chakra-ui/react'
-import { t } from 'i18next'
-import { useNavigate } from 'react-router-dom'
+import { Loader } from '@giphy/react-components'
+import { useTranslation } from 'react-i18next'
 
-import Loader from '@/components/ui/Loader'
-import { defaultProjectReward } from '@/defaults'
-import { useProjectRewardsAPI } from '@/modules/project/API/useProjectRewardsAPI'
 import { useProjectAtom } from '@/modules/project/hooks/useProjectAtom'
-import { getPath } from '@/shared/constants'
-import { useNotification } from '@/utils'
 
-import { ProjectRewardForm } from '../shared/ProjectRewardForm'
-import { ProjectRewardFormFinal } from '../shared/ProjectRewardFormFinal'
+import { ProjectRewardForm } from '../shared'
 
 export const RewardCreate = () => {
-  const navigate = useNavigate()
-  const toast = useNotification()
-
-  const { project, loading } = useProjectAtom()
-
-  const { createReward } = useProjectRewardsAPI()
-
-  const handleCreateReward = (props: any) => {
-    createReward.execute({
-      ...props,
-      onCompleted(data) {
-        toast.success({
-          title: 'Successfully created reward!',
-        })
-        navigate(getPath('projectRewardView', project.name, data.projectRewardCreate.id))
-      },
-      onError(error) {
-        toast.error({
-          title: 'Failed to create reward',
-          description: `${error}`,
-        })
-      },
-    })
-  }
+  const { t } = useTranslation()
+  const { loading } = useProjectAtom()
 
   if (loading) {
     return <Loader />
@@ -44,17 +16,7 @@ export const RewardCreate = () => {
 
   return (
     <VStack w="full" paddingBottom="120px">
-      <ProjectRewardFormFinal buttonText={t('Publish Reward')} titleText={t('Create Reward')} createOrUpdate="create" />
-
-      <ProjectRewardForm
-        hideBackbutton
-        buttonText={t('Publish Reward')}
-        titleText={t('Create Reward')}
-        createOrUpdate="create"
-        rewardData={defaultProjectReward}
-        rewardSave={handleCreateReward}
-        rewardSaving={createReward.loading}
-      />
+      <ProjectRewardForm buttonText={t('Publish Reward')} titleText={t('Create Reward')} createOrUpdate="create" />
     </VStack>
   )
 }
--- src/modules/project/pages1/projectView/views/rewards/views/RewardEdit.tsx
+++ src/modules/project/pages1/projectView/views/rewards/views/RewardEdit.tsx
@@ -1,95 +1,29 @@
-import { Button, VStack } from '@chakra-ui/react'
+import { VStack } from '@chakra-ui/react'
 import { t } from 'i18next'
-import { PiArrowLeft } from 'react-icons/pi'
-import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import Loader from '@/components/ui/Loader'
-import { TopNavContainerBar } from '@/modules/navigation/components/topNav'
-import { useProjectRewardsAPI } from '@/modules/project/API/useProjectRewardsAPI'
 import { useProjectAtom } from '@/modules/project/hooks/useProjectAtom'
-import { getPath } from '@/shared/constants'
-import { useProjectRewardQuery } from '@/types'
-import { useNotification } from '@/utils'
 
-import { ProjectRewardForm } from '../shared/ProjectRewardForm'
-import { ProjectRewardFormFinal } from '../shared/ProjectRewardFormFinal'
+import { ProjectRewardForm } from '../shared'
 
 export const RewardEdit = () => {
-  const navigate = useNavigate()
-  const toast = useNotification()
-
-  const { project, loading } = useProjectAtom()
+  const { loading } = useProjectAtom()
 
   const { rewardId } = useParams<{ rewardId: string }>()
 
-  const { loading: rewardLoading, data } = useProjectRewardQuery({
-    skip: !rewardId,
-    variables: {
-      getProjectRewardId: rewardId,
-    },
-  })
-
-  const { updateReward } = useProjectRewardsAPI()
-
-  const handleUpdateReward = (props: any) => {
-    updateReward.execute({
-      ...props,
-      onCompleted(data) {
-        toast.success({
-          title: 'Successfully updated!',
-          description: `Reward ${data.projectRewardUpdate.name} was successfully updated`,
-        })
-        navigate(getPath('projectRewardView', project.name, data.projectRewardUpdate.id))
-      },
-      onError(error) {
-        toast.error({
-          title: 'Failed to update reward',
-          description: `${error}`,
-        })
-      },
-    })
-  }
-
-  const reward = data?.getProjectReward
-
-  if (loading || rewardLoading || !reward) {
+  if (loading) {
     return <Loader />
   }
 
   return (
     <VStack w="full" paddingBottom="120px">
-      <TopNavContainerBar>
-        <Button
-          as={Link}
-          to={getPath('projectRewards', project?.name)}
-          size="lg"
-          variant="ghost"
-          colorScheme="neutral1"
-          leftIcon={<PiArrowLeft />}
-        >
-          {t('Back to rewards')}
-        </Button>
-        {/* <Button size={{ base: 'md', lg: 'lg' }} variant="solid" colorScheme="primary1">
-          {t('Publish')}
-        </Button> */}
-      </TopNavContainerBar>
-
-      <ProjectRewardFormFinal
+      <ProjectRewardForm
         buttonText={t('Update Reward')}
         titleText={t('Edit Reward')}
         createOrUpdate="update"
         isLaunch={false}
         rewardId={rewardId}
-      />
-
-      <ProjectRewardForm
-        hideBackbutton
-        buttonText={t('Update Reward')}
-        titleText={t('Edit Reward')}
-        createOrUpdate="update"
-        rewardData={reward}
-        rewardSave={handleUpdateReward}
-        rewardSaving={updateReward.loading}
       />
     </VStack>
   )
