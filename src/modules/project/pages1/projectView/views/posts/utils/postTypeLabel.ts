5c437f3d9b15c9f79196f82fffdd8ad3a39fdda4
Stelios Rammos <stelio.rammos@gmail.com>
2024-11-04 15:40:12 -0600

    fix: post type ordering

--- src/modules/project/pages1/projectView/views/posts/utils/postTypeLabel.ts
+++ src/modules/project/pages1/projectView/views/posts/utils/postTypeLabel.ts
@@ -3,13 +3,13 @@ import { PiFlagBannerFold, PiHandbag, PiMegaphoneSimple, PiMeteor, PiSiren, PiUs
 import { PostType } from '@/types'
 
 export const postTypeOptions = [
-  { label: 'Reward Update', value: PostType.RewardUpdate, icon: PiHandbag },
-  { label: 'Goal Update', value: PostType.GoalUpdate, icon: PiFlagBannerFold },
-  { label: 'Goal Reached', value: PostType.GoalReached, icon: PiFlagBannerFold },
   { label: 'Announcement', value: PostType.Announcement, icon: PiMegaphoneSimple },
   { label: 'Behind the scenes', value: PostType.BehindTheScenes, icon: PiSiren },
-  { label: 'Feedback Request', value: PostType.FeedbackRequest, icon: PiUserSound },
-  { label: 'New Goal', value: PostType.NewGoal, icon: PiFlagBannerFold },
-  { label: 'New Reward', value: PostType.NewReward, icon: PiHandbag },
+  { label: 'Feedback request', value: PostType.FeedbackRequest, icon: PiUserSound },
+  { label: 'Goal - New', value: PostType.NewGoal, icon: PiFlagBannerFold },
+  { label: 'Goal - Update', value: PostType.GoalUpdate, icon: PiFlagBannerFold },
+  { label: 'Goal - Reached', value: PostType.GoalReached, icon: PiFlagBannerFold },
   { label: 'Impact', value: PostType.Impact, icon: PiMeteor },
+  { label: 'Reward - New', value: PostType.NewReward, icon: PiHandbag },
+  { label: 'Reward - Update', value: PostType.RewardUpdate, icon: PiHandbag },
 ]
