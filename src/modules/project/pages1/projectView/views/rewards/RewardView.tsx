55861a2aa5070f8dde3f39d5fe6ef3b72061da74
sajald77 <sazzaldulal@gmail.com>
2024-11-06 19:14:51 +0545

    fix: don't show reward updates when there are no updates

--- src/modules/project/pages1/projectView/views/rewards/RewardView.tsx
+++ src/modules/project/pages1/projectView/views/rewards/RewardView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 import { Badge, Box, Button, HStack, SkeletonText, VStack } from '@chakra-ui/react'
 import { t } from 'i18next'
 import { PiArrowLeft, PiEyeSlash } from 'react-icons/pi'
@@ -190,12 +191,14 @@ export const RewardView = () => {
               <MarkdownField preview content={reward.description || ''} />
             </HStack>
           )}
-          <VStack w="full" spacing={2} alignItems="start">
-            <Body size="xl" bold>
-              {t('Reward Updates')}
-            </Body>
-            <PostsUpdates posts={reward.posts} />
-          </VStack>
+          {reward.posts.length > 0 && (
+            <VStack w="full" spacing={2} alignItems="start">
+              <Body size="xl" bold>
+                {t('Reward Updates')}
+              </Body>
+              <PostsUpdates posts={reward.posts} />
+            </VStack>
+          )}
         </VStack>
       </CardLayout>
       <BottomNavBarContainer direction="column">
