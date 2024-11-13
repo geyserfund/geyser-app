3127759e0c464d5894b93904e99a6547d94eab6d
Stelios Rammos <stelio.rammos@gmail.com>
2024-11-13 11:25:37 -0600

    fix: post publish without email send options

--- src/modules/project/pages1/projectView/views/posts/components/PublishModal.tsx
+++ src/modules/project/pages1/projectView/views/posts/components/PublishModal.tsx
@@ -113,10 +113,12 @@ export const PublishModal = ({
 
   const handlePostPublish = async () => {
     postPublish({
-      emailSendOptions: {
-        segment: sendTo as EmailSubscriberSegment,
-        projectRewardUUIDs: selectedRewards.length > 0 ? selectedRewards.map((reward) => reward.uuid) : undefined,
-      },
+      ...(sendTo && {
+        emailSendOptions: {
+          segment: sendTo as EmailSubscriberSegment,
+          projectRewardUUIDs: selectedRewards.length > 0 ? selectedRewards.map((reward) => reward.uuid) : undefined,
+        },
+      }),
       onCompleted() {
         navigate(getPath('projectPostView', project.name, post?.id), { state: { justPublished: true } })
       },
