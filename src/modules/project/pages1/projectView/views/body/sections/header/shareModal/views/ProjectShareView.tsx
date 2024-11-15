d07eda830e835007e0e18de09c80d578e665f70d
Stelios Rammos <stelio.rammos@gmail.com>
2024-11-15 13:49:34 -0600

    fix: remove unused import

--- src/modules/project/pages1/projectView/views/body/sections/header/shareModal/views/ProjectShareView.tsx
+++ src/modules/project/pages1/projectView/views/body/sections/header/shareModal/views/ProjectShareView.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, IconButton, Input, Link, Tooltip, useClipboard, VStack } from '@chakra-ui/react'
+import { Button, HStack, IconButton, Link, Tooltip, useClipboard, VStack } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { PiCopy, PiShareFat } from 'react-icons/pi'
 
