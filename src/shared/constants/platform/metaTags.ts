e9165df8c380036347d68956d02486e19dee345a
Stelios Rammos <stelio.rammos@gmail.com>
2024-11-21 11:33:25 -0600

    fix: replace Geyser Fund with Geyser

--- src/shared/constants/platform/metaTags.ts
+++ src/shared/constants/platform/metaTags.ts
@@ -1,4 +1,4 @@
-export const DefaultMetaTitle = 'Geyser Fund - Crowdfunding with Bitcoin'
+export const DefaultMetaTitle = 'Geyser - Crowdfunding with Bitcoin'
 export const DefaultMetaImage = 'https://storage.googleapis.com/geyser-projects-media/app/seo.png'
 export const DefaultMetaDescription =
   'Drop some sats on the projects you want to see come to life. Join the crowdfund now!'
