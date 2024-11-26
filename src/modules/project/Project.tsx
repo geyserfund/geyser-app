ad26ac698984ed780d9466bd52b5e7a8e05637dd
sajald77 <sazzaldulal@gmail.com>
2024-11-26 14:03:17 -0500

    fix: remove project ids redirect

--- src/modules/project/Project.tsx
+++ src/modules/project/Project.tsx
@@ -1,8 +1,6 @@
 import { useSetAtom } from 'jotai'
 import { useEffect } from 'react'
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
-
-import { getPath } from '@/shared/constants'
+import { useParams, useSearchParams } from 'react-router-dom'
 
 import { ProjectProvider } from './context'
 import { FundingProviderWithProjectContext } from './context/FundingProvider'
@@ -10,33 +8,17 @@ import { addProjectAffiliateAtom } from './pages1/projectView/state/affiliateAto
 import { addProjectHeroAtom } from './pages1/projectView/state/heroAtom'
 import { ProjectContainer } from './ProjectContainer'
 
-const ProjectIdsRedirects = [
-  {
-    from: 'wecollectdonationsforsacrificesofeidaladhapalestine',
-    to: 'bitcoinforpalestine',
-  },
-]
-
 export const Project = () => {
   const params = useParams<{ projectName: string }>()
   const { projectName } = params
 
   const [searchParams] = useSearchParams()
 
-  const navigate = useNavigate()
-
   const addRefferal = useSetAtom(addProjectAffiliateAtom)
   const addHeroId = useSetAtom(addProjectHeroAtom)
 
   const affiliateId = searchParams.get('refId')
   const heroId = searchParams.get('hero')
-
-  useEffect(() => {
-    const redirect = ProjectIdsRedirects.find((item) => item.from === projectName)
-    if (redirect) {
-      navigate(getPath('project', redirect.to))
-    }
-  }, [projectName, navigate])
 
   useEffect(() => {
     if (affiliateId && projectName) {
