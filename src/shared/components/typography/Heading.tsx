287bc1c3f98563e1f2ad272e68733f3c3c9534ec
sajald77 <sazzaldulal@gmail.com>
2024-12-04 00:01:49 -0500

    chore: cleanup

--- src/shared/components/typography/Heading.tsx
+++ src/shared/components/typography/Heading.tsx
@@ -1,33 +1,35 @@
-import { ComponentWithAs, Heading, HeadingProps } from '@chakra-ui/react'
+import { Heading, HeadingProps } from '@chakra-ui/react'
 
 import { getFontColor, getFontWeight, TextColorProps, TextWeightProps } from './common'
 
-interface HeaderProps extends HeadingProps, TextWeightProps, TextColorProps {}
+interface HeaderProps extends HeadingProps, TextWeightProps, TextColorProps {
+  to?: string
+}
 
 /** H1 heading, default size: 4xl / 36px */
-export const H1: ComponentWithAs<'h1', HeadingProps> = (props) => {
+export const H1 = (props: HeaderProps) => {
   return <BaseHeading as="h1" size="4xl" {...props} />
 }
 
 /** H2 heading, default size: 2xl / 24px */
-export const H2: ComponentWithAs<'h2', HeadingProps> = (props) => {
+export const H2 = (props: HeaderProps) => {
   return <BaseHeading as="h2" size="2xl" {...props} />
 }
 
 /** H3 heading, default size: lg / 18px */
-export const H3: ComponentWithAs<'h3', HeadingProps> = (props) => {
+export const H3 = (props: HeaderProps) => {
   return <BaseHeading as="h3" size="lg" {...props} />
 }
 
-export const H4: ComponentWithAs<'h4', HeadingProps> = (props) => {
+export const H4 = (props: HeaderProps) => {
   return <BaseHeading as="h4" {...props} />
 }
 
-export const H5: ComponentWithAs<'h5', HeadingProps> = (props) => {
+export const H5 = (props: HeaderProps) => {
   return <BaseHeading as="h5" {...props} />
 }
 
-export const H6: ComponentWithAs<'h6', HeadingProps> = (props) => {
+export const H6 = (props: HeaderProps) => {
   return <BaseHeading as="h6" {...props} />
 }
 
