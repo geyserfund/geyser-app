13189f4a3d472867fc7cf0bccbe75aab54815f5f
sajald77 <sazzaldulal@gmail.com>
2025-03-18 11:53:29 -0400

    fix: badge tooltip and colormode fix

--- src/modules/profile/pages/profilePage/views/account/views/badges/VerifiedBadge.tsx
+++ src/modules/profile/pages/profilePage/views/account/views/badges/VerifiedBadge.tsx
@@ -1,6 +1,7 @@
-import { Icon, IconProps, Tooltip } from '@chakra-ui/react'
+import { Box, HStack, Icon, IconProps, Tooltip } from '@chakra-ui/react'
 import { PiSealCheckFill } from 'react-icons/pi'
 
+import { lightModeColors } from '@/shared/styles/colors.ts'
 import { UserForProfilePageFragment } from '@/types/index.ts'
 
 type VerifiedBadgeProps = {
@@ -10,8 +11,11 @@ type VerifiedBadgeProps = {
 export const VerifiedBadge = ({ user, ...props }: VerifiedBadgeProps) => {
   if (user.complianceDetails?.verifiedDetails?.identity?.verified) {
     return (
-      <Tooltip label="This user has verified their identity with Geyser.">
-        <Icon as={PiSealCheckFill} color="primary1.9" {...props} />
+      <Tooltip label="This user has verified their identity with Geyser." placement="top">
+        <HStack position="relative" alignItems="center" justifyContent="center">
+          <Box position="absolute" w="50%" h="50%" bg={lightModeColors.utils.text} borderRadius="full" color="" />
+          <Icon as={PiSealCheckFill} color="primary1.9" {...props} zIndex={1} />
+        </HStack>
       </Tooltip>
     )
   }
