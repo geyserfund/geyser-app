5b7e6dd7365b5f180a2a674e850b96b40449d5b2
sajald77 <sazzaldulal@gmail.com>
2025-03-19 14:50:52 -0400

    fix: show redirection notice after payment waiting

--- src/modules/project/pages1/projectFunding/views/fundingPayment/views/paymentFiatSwap/PaymentFiatSwap.tsx
+++ src/modules/project/pages1/projectFunding/views/fundingPayment/views/paymentFiatSwap/PaymentFiatSwap.tsx
@@ -11,7 +11,6 @@ import { FiatSwapAwaitingPayment } from './components/FiatSwapAwaitingPayment.ts
 import { FiatSwapContributorNotVerified } from './components/FiatSwapContributorNotVerified.tsx'
 import { FiatSwapForm } from './components/FiatSwapForm.tsx'
 import { FiatSwapOwnerNotVerified } from './components/FiatSwapOwnerNotVerified.tsx'
-import { RedirectionNotice } from './components/RedirectionNotice.tsx'
 import { FiatSwapStatus, fiatSwapStatusAtom } from './fiatSwapStatus.ts'
 
 export const PaymentFiatSwap = () => {
@@ -52,7 +51,6 @@ export const PaymentFiatSwap = () => {
   return (
     <VStack w="full" spacing={6}>
       {renderFiatSwapPayment()}
-      <RedirectionNotice />
       {isProjectOwnerVerified && <FiatSwapContributorNotVerified />}
     </VStack>
   )
