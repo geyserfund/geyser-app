0ae6dd3fdb25573f9aec0dfcea6987f700d60c6e
sajald77 <sazzaldulal@gmail.com>
2025-03-20 15:19:51 -0400

    fix: update map button to include tooltip

--- src/modules/navigation/platformNavBar/components/MapButton.tsx
+++ src/modules/navigation/platformNavBar/components/MapButton.tsx
@@ -1,19 +1,22 @@
-import { Icon, IconButton, Link } from '@chakra-ui/react'
+import { Icon, IconButton, Link, Tooltip } from '@chakra-ui/react'
+import { t } from 'i18next'
 import { PiGlobeHemisphereWestFill } from 'react-icons/pi'
 
 const MAP_URL = 'https://map.geyser.fund'
 
 export const MapButton = () => {
   return (
-    <IconButton
-      as={Link}
-      href={MAP_URL}
-      isExternal
-      icon={<Icon as={PiGlobeHemisphereWestFill} fontSize="28px" />}
-      aria-label="Map"
-      variant="soft"
-      size="lg"
-      _hover={{ '& > svg': { color: 'primary1.9' } }}
-    />
+    <Tooltip label={t('Browse map')}>
+      <IconButton
+        as={Link}
+        href={MAP_URL}
+        isExternal
+        icon={<Icon as={PiGlobeHemisphereWestFill} fontSize="28px" />}
+        aria-label="Map"
+        variant="soft"
+        size="lg"
+        _hover={{ '& > svg': { color: 'primary1.9' } }}
+      />
+    </Tooltip>
   )
 }
