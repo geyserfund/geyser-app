380712abea81b9361d0bc755d1409e800c6b6707
sajald77 <sazzaldulal@gmail.com>
2025-04-01 11:36:39 -0400

    fix: storybook build

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  stories: ['../../../**/*.mdx', '../../../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@chakra-ui/storybook-addon',
     '@storybook/addon-onboarding',
