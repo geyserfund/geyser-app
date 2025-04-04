a0d10dca18963e7f4889212d57a9e6edb118f206
sajald77 <sazzaldulal@gmail.com>
2025-04-04 12:50:15 -0400

    feat: add wallet popup when clicking on wallet

--- src/modules/project/pages1/projectFunding/views/fundingPayment/views/PaymentLightning.tsx
+++ src/modules/project/pages1/projectFunding/views/fundingPayment/views/PaymentLightning.tsx
@@ -6,6 +6,7 @@ import { PiCopy } from 'react-icons/pi'
 import { useListenFundingContributionSuccess } from '@/modules/project/funding/hooks/useListenFundingContributionSuccess'
 import { fundingPaymentDetailsAtom } from '@/modules/project/funding/state/fundingPaymentAtom.ts'
 import { useCopyToClipboard } from '@/shared/utils/hooks/useCopyButton'
+import { useNotification } from '@/utils/index.ts'
 
 import { QRCodeComponent } from '../components/QRCodeComponent'
 import { TotalAmountToPay } from '../components/TotalAmountToPay'
@@ -13,6 +14,8 @@ import { WaitingForPayment } from '../components/WaitingForPayment'
 
 export const PaymentLightning = () => {
   useListenFundingContributionSuccess()
+
+  const toast = useNotification()
 
   const fundingPaymentDetails = useAtomValue(fundingPaymentDetailsAtom)
 
@@ -22,9 +25,36 @@ export const PaymentLightning = () => {
     return null
   }
 
+  const handleCopy = () => {
+    onCopy()
+    toast.success({
+      title: t('Invoice copied to clipboard'),
+    })
+  }
+
+  /** Handles opening lightning payment in mobile wallets */
+  const handleLightningPayment = () => {
+    const paymentRequest = fundingPaymentDetails.lightning?.paymentRequest
+    if (!paymentRequest) return
+
+    // Lightning invoice URI scheme
+    const lightningUri = `lightning:${paymentRequest}`
+
+    // Only attempt to open lightning wallets on mobile devices
+    if (/Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+      window.location.href = lightningUri
+    } else {
+      handleCopy()
+    }
+  }
+
   return (
     <VStack w="full">
-      <QRCodeComponent value={fundingPaymentDetails.lightning.paymentRequest} onClick={onCopy} isColored={hasCopied} />
+      <QRCodeComponent
+        value={fundingPaymentDetails.lightning.paymentRequest}
+        onClick={handleLightningPayment}
+        isColored={hasCopied}
+      />
       <TotalAmountToPay />
       <VStack w="full" spacing={6} pt={4}>
         <WaitingForPayment />
