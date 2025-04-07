34228bcab103b0225c6f565d5fba94c558f32107
sajald77 <sazzaldulal@gmail.com>
2025-04-07 18:33:51 -0400

    fix: matomo tag manager stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'jotai'
-import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import { client } from './config/apollo-client'
 import { CacheBuster } from './config/CacheBuster.tsx'
 import { Head } from './config/Head'
-import { configMatomo } from './config/matomo'
 import { AuthProvider, ChakraThemeProvider, ServiceWorkerProvider } from './context'
 import { BtcProvider } from './context/btc'
 import { FilterProvider } from './context/filter'
 
 export const App = () => {
-  useEffect(() => {
-    configMatomo()
-  }, [])
-
   return (
     <Provider>
       <ChakraProvider>
