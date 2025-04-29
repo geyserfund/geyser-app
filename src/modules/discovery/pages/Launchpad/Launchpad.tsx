ad8462dfc5910c9ad752ea8966e29d41fb408d59
sajald77 <sazzaldulal@gmail.com>
2025-04-29 13:34:39 -0400

    feat: add launchpad seo

--- src/modules/discovery/pages/Launchpad/Launchpad.tsx
+++ src/modules/discovery/pages/Launchpad/Launchpad.tsx
@@ -1,10 +1,14 @@
 import { VStack } from '@chakra-ui/react'
 
+import { Head } from '@/config/Head.tsx'
+import { LaunchpadSeoImageUrl } from '@/shared/constants/index.ts'
+
+import { LaunchpadProjects } from './components/LaunchpadProjects.tsx'
 import { LaunchpadTitle } from './components/LaunchpadTitle.tsx'
-import { LaunchpadProjects } from './LaunchpadProjects.tsx'
 export const Launchpad = () => {
   return (
     <VStack w="full" spacing={4}>
+      <Head title={'Launchpad'} description={'Launchpad'} image={LaunchpadSeoImageUrl} type="article" />
       <LaunchpadTitle />
       <LaunchpadProjects />
     </VStack>
