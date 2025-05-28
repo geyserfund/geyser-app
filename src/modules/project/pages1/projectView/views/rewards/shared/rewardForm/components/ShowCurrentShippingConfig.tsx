dbad3c8d3c62663690dda9efece69156f9668079
sajald77 <sazzaldulal@gmail.com>
2025-05-28 12:14:20 -0400

    fix: make minor updates to make the shipping fees display cleaner

--- src/modules/project/pages1/projectView/views/rewards/shared/rewardForm/components/ShowCurrentShippingConfig.tsx
+++ src/modules/project/pages1/projectView/views/rewards/shared/rewardForm/components/ShowCurrentShippingConfig.tsx
@@ -61,109 +61,111 @@ export const ShowCurrentShippingConfig = ({
           width: '100%',
         }}
       >
-        <VStack
-          w="full"
-          maxWidth="550px"
-          alignItems="flex-start"
-          spacing={4}
-          padding={5}
-          borderRadius="8px"
-          backgroundColor="neutral1.3"
-        >
-          <VStack w="full" spacing={0}>
-            <HStack w="full" justifyContent="space-between" spacing={0}>
-              <Box flex={1.5}>
-                <Body size="sm" medium>
-                  {t('Fee model')}:
-                </Body>
-              </Box>
-              <Box flex={isIncremental ? 2 : 1}>
-                <Body size="sm" light display={'inline-block'}>
-                  {
-                    ProjectShippingConfigTypeOptions.find((option) => option.value === selectedShippingConfig?.type)
-                      ?.label
-                  }
-                </Body>{' '}
-                <TooltipPopover
-                  content={
-                    <VStack alignItems="flex-start">
-                      {ProjectShippingConfigTypeOptions.find(
-                        (option) => option.value === selectedShippingConfig?.type,
-                      )?.info.map((info) => (
-                        <Body size="sm" key={info}>
-                          {info}
-                        </Body>
-                      ))}
-                    </VStack>
-                  }
-                >
-                  <Box display={'inline-block'}>
-                    <Icon as={PiInfo} />
-                  </Box>
-                </TooltipPopover>
-              </Box>
-            </HStack>
-            <HStack w="full" justifyContent="space-between" alignItems="flex-start" spacing={0}>
-              <Box flex={1.5}>
-                <Body size="sm" medium>
-                  {t('Shipping availability')}:
-                </Body>
-              </Box>
-              <Box flex={isIncremental ? 2 : 1}>
-                <Body size="sm" light>
-                  {selectedShippingConfig?.globalShipping ? t('Worldwide') : t('Specific countries listed below')}
-                </Body>
-              </Box>
-            </HStack>
-          </VStack>
+        <VStack w="full" maxWidth="622px">
+          <VStack
+            w="full"
+            maxWidth="622px"
+            alignItems="flex-start"
+            spacing={4}
+            padding={5}
+            borderRadius="8px"
+            backgroundColor="neutral1.3"
+          >
+            <VStack w="full" spacing={0}>
+              <HStack w="full" justifyContent="space-between" spacing={0}>
+                <Box flex={1.5}>
+                  <Body size="sm" medium>
+                    {t('Fee model')}:
+                  </Body>
+                </Box>
+                <Box flex={isIncremental ? 2 : 1}>
+                  <Body size="sm" light display={'inline-block'}>
+                    {
+                      ProjectShippingConfigTypeOptions.find((option) => option.value === selectedShippingConfig?.type)
+                        ?.label
+                    }
+                  </Body>{' '}
+                  <TooltipPopover
+                    content={
+                      <VStack alignItems="flex-start">
+                        {ProjectShippingConfigTypeOptions.find(
+                          (option) => option.value === selectedShippingConfig?.type,
+                        )?.info.map((info) => (
+                          <Body size="sm" key={info}>
+                            {info}
+                          </Body>
+                        ))}
+                      </VStack>
+                    }
+                  >
+                    <Box display={'inline-block'}>
+                      <Icon as={PiInfo} />
+                    </Box>
+                  </TooltipPopover>
+                </Box>
+              </HStack>
+              <HStack w="full" justifyContent="space-between" alignItems="flex-start" spacing={0}>
+                <Box flex={1.5}>
+                  <Body size="sm" medium>
+                    {t('Shipping availability')}:
+                  </Body>
+                </Box>
+                <Box flex={isIncremental ? 2 : 1}>
+                  <Body size="sm" light>
+                    {selectedShippingConfig?.globalShipping ? t('Worldwide') : t('Specific countries listed below')}
+                  </Body>
+                </Box>
+              </HStack>
+            </VStack>
 
-          <VStack w="full" spacing={2} padding={0}>
-            <HStack w="full" justifyContent="space-between" borderBottom="1px solid" borderColor="neutral1.6">
-              <Box flex={1.5}>
-                <Body size="sm" medium>
-                  {t('Rates')}
-                </Body>
-              </Box>
-              <Box flex={1}>
-                <Body size="sm" medium>
-                  {t('Base rate')}
-                </Body>
-              </Box>
-              {isIncremental && (
+            <VStack w="full" spacing={2} padding={0}>
+              <HStack w="full" justifyContent="space-between">
+                <Box flex={1.5}>
+                  <Body size="sm" medium>
+                    {t('Rates')}
+                  </Body>
+                </Box>
                 <Box flex={1}>
                   <Body size="sm" medium>
-                    {t('Increment rate')}
+                    {t('Base rate')}
                   </Body>
                 </Box>
-              )}
-            </HStack>
-
-            {selectedShippingConfig?.shippingRates?.map((rate) => (
-              <HStack w="full" key={rate.country} justifyContent="space-between">
-                <Box flex={1.5}>
-                  <Body size="sm">{country.find((c) => c.code === rate.country)?.name || rate.country}</Body>
-                </Box>
-
-                <Box flex={1}>
-                  <Body size="sm" light>
-                    {rate.baseRate ? formatAmount(rate.baseRate, 'USDCENT') : t('Same as Default')}
-                  </Body>
-                </Box>
-
                 {isIncremental && (
                   <Box flex={1}>
-                    <Body size="sm" light>
-                      {rate.incrementRate ? formatAmount(rate.incrementRate, 'USDCENT') : t('Same as Default')}
+                    <Body size="sm" medium>
+                      {t('Increment rate')}
                     </Body>
                   </Box>
                 )}
               </HStack>
-            ))}
+
+              {selectedShippingConfig?.shippingRates?.map((rate) => (
+                <HStack w="full" key={rate.country} justifyContent="space-between">
+                  <Box flex={1.5}>
+                    <Body size="sm">{country.find((c) => c.code === rate.country)?.name || rate.country}</Body>
+                  </Box>
+
+                  <Box flex={1}>
+                    <Body size="sm" light>
+                      {rate.baseRate ? formatAmount(rate.baseRate, 'USDCENT') : t('Same as Default')}
+                    </Body>
+                  </Box>
+
+                  {isIncremental && (
+                    <Box flex={1}>
+                      <Body size="sm" light>
+                        {rate.incrementRate ? formatAmount(rate.incrementRate, 'USDCENT') : t('Same as Default')}
+                      </Body>
+                    </Box>
+                  )}
+                </HStack>
+              ))}
+            </VStack>
           </VStack>
           <Button
             variant="outline"
             colorScheme="neutral1"
-            size="md"
+            size="lg"
             width="full"
             rightIcon={<PiPencil />}
             onClick={() => shippingFeeModal.onOpen({ isEdit: true })}
