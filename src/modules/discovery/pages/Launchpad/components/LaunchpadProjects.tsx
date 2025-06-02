d88f2e848ebbc5ee8eb27f57e7409b34f1f4d512
sajald77 <sazzaldulal@gmail.com>
2025-06-02 10:38:32 -0400

    fix: launchpad page by using only one query

--- src/modules/discovery/pages/Launchpad/components/LaunchpadProjects.tsx
+++ src/modules/discovery/pages/Launchpad/components/LaunchpadProjects.tsx
@@ -1,7 +1,8 @@
-import { SimpleGrid } from '@chakra-ui/react'
+import { GridItem, SimpleGrid } from '@chakra-ui/react'
 
-import { ProjectCategory, ProjectSubCategory } from '@/types/index.ts'
+import { ProjectCategory, ProjectStatus, ProjectSubCategory, useProjectsForLaunchpadPageQuery } from '@/types/index.ts'
 
+import { LaunchpadProjectItemSkeleton } from './LaunchpadProjectItem.tsx'
 import { LaunchpadProjectsByCategory } from './LaunchpadProjectsByCategory.tsx'
 
 const listOfItemsToShow = [
@@ -32,11 +33,54 @@ const listOfItemsToShow = [
 ]
 
 export const LaunchpadProjects = () => {
+  const { data, loading } = useProjectsForLaunchpadPageQuery({
+    fetchPolicy: 'network-only',
+    variables: {
+      input: {
+        where: {
+          status: ProjectStatus.PreLaunch,
+        },
+      },
+    },
+  })
+
+  const projects = data?.projectsGet.projects ? [...data.projectsGet.projects] : []
+
+  const projectsByCategory = listOfItemsToShow.map((item) => ({
+    ...item,
+    projects: projects.filter(
+      (project) =>
+        (item.category && project.category === item.category) ||
+        (item.subCategory && project.subCategory === item.subCategory),
+    ),
+  }))
+
+  console.log(projectsByCategory)
+
   return (
     <SimpleGrid w="full" columns={{ base: 1, sm: 2, md: 3, lg: 3, xl: 4 }} spacingX="20px" spacingY="20px">
-      {listOfItemsToShow.map((item, index) => {
-        return <LaunchpadProjectsByCategory key={index} category={item.category} subCategory={item.subCategory} />
-      })}
+      {loading
+        ? [...Array(4).keys()].map((key) => {
+            return (
+              <GridItem key={key}>
+                <LaunchpadProjectItemSkeleton />
+              </GridItem>
+            )
+          })
+        : projectsByCategory.map((item, index) => {
+            if (item.projects.length === 0) {
+              return null
+            }
+
+            return (
+              <LaunchpadProjectsByCategory
+                key={index}
+                category={item.category}
+                subCategory={item.subCategory}
+                projects={item.projects}
+              />
+            )
+          })}
     </SimpleGrid>
   )
 }
