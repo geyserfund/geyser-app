8b23d5aa06dfd4b0d4647f22e95142623c8659b3
sajald77 <sazzaldulal@gmail.com>
2026-01-14 12:30:03 -0500

    fix: update  campaign page

---
+++ src/modules/discovery/pages/landing/constants.ts
@@ -0,0 +1,6 @@
+export const CampaignsAlignIncentivesImageUrl =
+  'https://storage.googleapis.com/geyser-projects-media/app/campaigns/campaign_align_incentives.png'
+export const CampaignsGoGlobalImageUrl =
+  'https://storage.googleapis.com/geyser-projects-media/app/campaigns/campaign_go_global.png'
+export const CampaignsFailFastImageUrl =
+  'https://storage.googleapis.com/geyser-projects-media/app/campaigns/campagin_fail_fast.png'
