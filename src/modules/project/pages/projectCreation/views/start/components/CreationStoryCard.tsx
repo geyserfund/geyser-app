d433111f6e000be98885fa306a79e4ef56c87250
sajald77 <sazzaldulal@gmail.com>
2026-01-15 09:12:24 -0500

    fix: final adjustments

--- src/modules/project/pages/projectCreation/views/start/components/CreationStoryCard.tsx
+++ src/modules/project/pages/projectCreation/views/start/components/CreationStoryCard.tsx
@@ -1,5 +1,7 @@
 import { Box, Image, Link } from '@chakra-ui/react'
 
+import { CardLayout } from '@/shared/components/layouts/CardLayout.tsx'
+
 type StoryCard = {
   link: string
   image: string
@@ -8,23 +10,21 @@ type StoryCard = {
 /** Success Stories section with project cards */
 export const CreationStoryCard = ({ link, image }: StoryCard) => {
   return (
-    <Box
+    <CardLayout
+      dense
       as={Link}
       isExternal
       href={link}
-      borderRadius="md"
       overflow="hidden"
       bg="white"
       boxShadow="sm"
       cursor="pointer"
-      border="1px solid"
-      borderColor="neutral1.6"
       transition="transform 0.2s"
       _hover={{ transform: 'translateY(-4px)', boxShadow: 'xl' }}
     >
       <Box height="auto" width="100%" aspectRatio="1/1.4" bg="neutral.200">
         <Image src={image} alt={link} width="100%" height="100%" objectFit="cover" />
       </Box>
-    </Box>
+    </CardLayout>
   )
 }
