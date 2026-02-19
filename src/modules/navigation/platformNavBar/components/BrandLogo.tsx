3c826ecc84b0679ea492b0fafd54af913eb9d60d
Stelios Rammos <stelio.rammos@gmail.com>
2026-02-19 12:34:32 -0300

    feat: add env pill next to nav logo

--- src/modules/navigation/platformNavBar/components/BrandLogo.tsx
+++ src/modules/navigation/platformNavBar/components/BrandLogo.tsx
@@ -1,18 +1,54 @@
-import { Box, Image, useColorModeValue } from '@chakra-ui/react'
+import { Badge, Box, HStack, Image, useColorModeValue } from '@chakra-ui/react'
+import { t } from 'i18next'
 import { Link } from 'react-router'
 
 import LogoDark from '@/assets/logo-dark.svg'
 import LogoLight from '@/assets/logo-light.svg'
-import { getPath, LogoNameDark, LogoNameLight, LogoOutline } from '@/shared/constants'
+import { __development__, __staging__, getPath, LogoNameDark, LogoNameLight, LogoOutline } from '@/shared/constants'
+
+const EnvironmentTag = () => {
+  if (!(__development__ || __staging__)) {
+    return null
+  }
+
+  const isDevelopment = __development__
+  const desktopLabel = isDevelopment ? t('Development') : t('Staging')
+  const mobileLabel = isDevelopment ? 'DEV' : 'STG'
+
+  return (
+    <Badge
+      colorScheme={isDevelopment ? 'blue' : 'orange'}
+      variant="subtle"
+      borderRadius="full"
+      px={{ base: 2, lg: 3 }}
+      py={1}
+      fontSize={{ base: '2xs', lg: 'xs' }}
+      fontWeight={700}
+      lineHeight={1}
+      textTransform="uppercase"
+      whiteSpace="nowrap"
+    >
+      <Box as="span" display={{ base: 'none', sm: 'inline' }}>
+        {desktopLabel}
+      </Box>
+      <Box as="span" display={{ base: 'inline', sm: 'none' }}>
+        {mobileLabel}
+      </Box>
+    </Badge>
+  )
+}
 
 export const BrandLogo = ({ showOutline = false }: { showOutline?: boolean }) => {
   const imagesrc = useColorModeValue(LogoDark, LogoLight)
 
   return (
     <Link to={getPath('landingPage')} style={{ height: '100%' }}>
-      <Box h="100%">
-        <Image src={showOutline ? LogoOutline : imagesrc} height="100%" width="auto" objectFit="contain" />
-      </Box>
+      <HStack h="100%" spacing={{ base: 1, lg: 2 }}>
+        <Box h="100%">
+          <Image src={showOutline ? LogoOutline : imagesrc} height="100%" width="auto" objectFit="contain" alt="Geyser logo" />
+        </Box>
+        <EnvironmentTag />
+      </HStack>
     </Link>
   )
 }
@@ -21,9 +57,12 @@ export const BrandLogoFull = () => {
   const imageUrl = useColorModeValue(LogoNameDark, LogoNameLight)
   return (
     <Link to={getPath('landingPage')} style={{ height: '100%' }}>
-      <Box h="100%">
-        <Image src={imageUrl} alt={'Brand logo image'} height="100%" width="auto" objectFit="contain" />
-      </Box>
+      <HStack h="100%" spacing={{ base: 1, lg: 2 }}>
+        <Box h="100%">
+          <Image src={imageUrl} alt={'Brand logo image'} height="100%" width="auto" objectFit="contain" />
+        </Box>
+        <EnvironmentTag />
+      </HStack>
     </Link>
   )
 }
