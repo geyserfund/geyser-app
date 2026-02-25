859de7957824b8e375aab121b105edc47a69005c
sajald77 <sazzaldulal@gmail.com>
2026-02-25 09:34:22 -0500

    fix: bump version to 0.27.5 and add SEO images for impact funds

--- src/modules/impactFunds/utils/constants.ts
+++ src/modules/impactFunds/utils/constants.ts
@@ -1,2 +1,10 @@
 export const IMPACT_FUNDS_IMAGE_URL =
   'https://storage.googleapis.com/geyser-projects-media/app/impactfunds/impact_funds.webp'
+
+export const IMPACT_FUND_DETAILS_SEO_IMAGES = {
+  'culture-impact-fund': 'https://storage.googleapis.com/geyser-projects-media/app/impactfunds/culture_impact.png',
+  'latam-impact-fund': 'https://storage.googleapis.com/geyser-projects-media/app/impactfunds/latam_impact_funds.png',
+  'education-impact-fund': 'https://storage.googleapis.com/geyser-projects-media/app/impactfunds/education_posts.png',
+  'circular-economies-impact-fund':
+    'https://storage.googleapis.com/geyser-projects-media/app/impactfunds/circular_economies.png',
+}
