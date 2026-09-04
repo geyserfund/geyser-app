bd88752b3a6ef9073ed67e3f0f04ae970edf1960
Stelios Rammos <stelio.rammos@gmail.com>
2026-09-04 20:08:29 -0300

    chore: remove Matomo tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'jotai'
-import { useEffect } from 'react'
 import { Outlet } from 'react-router'
 
 import { client } from './config/apollo-client'
 import { CacheBuster } from './config/CacheBuster.tsx'
 import { Head } from './config/Head'
-import { configMatomo } from './config/matomo'
 import { AuthProvider, ChakraThemeProvider, ServiceWorkerProvider } from './context'
 import { FilterProvider } from './context/filter'
 import { ReferralCapture } from './shared/components/ReferralCapture.tsx'
 
 export const App = () => {
-  useEffect(() => {
-    configMatomo()
-  }, [])
-
   return (
     <Provider>
       <ChakraProvider>
